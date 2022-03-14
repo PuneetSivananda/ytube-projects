341a665d6349383a44bcefec3a90b656138edf50
PuneetSivananda <psivananda@chatsoft.com>
2022-03-14 22:58:45 +0530

    feat: custom cli bar chart

--- bar_chart.py
+++ bar_chart.py
@@ -1,12 +1,13 @@
 from collections import Counter
 
+
 def print_ascii_bar_chart(data, symbol="#"):
     counter = Counter(data).most_common()
     chart = {category: symbol * frequency for category, frequency in counter}
     max_len = max(len(category) for category in chart)
     for category, frequency in chart.items():
         padding = (max_len - len(category)) * " "
-        print(f"{category}{padding} |{frequency}")
+        print("{0}{1} | {2}".format(category, padding, frequency))
 
 
 letters = "mississippimississippimississippimississippi"
