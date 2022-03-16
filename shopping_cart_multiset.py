b22b8244cf348504d91945e9dcf985f6f716ddea
PuneetSivananda <psivananda@chatsoft.com>
2022-03-16 21:56:43 +0530

    feat: added total items in shopping cart

--- shopping_cart_multiset.py
+++ shopping_cart_multiset.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 from collections import Counter
+from re import sub
 
 prices = {
     "course": 97.99,
     "book": 54.99,
-    "wallpaper": 4.99
+    "wallpaper": 4.99,
+    "total": 0
 }
 
 cart = Counter(course=1, book=3, wallpaper=2)
@@ -13,3 +15,7 @@ for product, units in cart.items():
     subtotal = units * prices[product]
     price = prices[product]
     print(f"{product:10}: ${price:7.2f} x {units} = {subtotal:7.2f}")
+    prices["total"] += subtotal
+
+
+print(f"Total: {prices['total']:7.2f}")
