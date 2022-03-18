a62d63652a0b06fb3bb240b65bd46dcfa783abeb
PuneetSivananda <psivananda@chatsoft.com>
2022-03-18 12:33:51 +0530

    todo: finish allConstruct tabulation

--- node/tabulation/allConstruct.js
+++ node/tabulation/allConstruct.js
@@ -1,16 +1,29 @@
 const allConstruct = (target, wordBank)=>{
-    const table =Array(target.length +1).fill(0);
+    const table = Array(target.length + 1)
+        .fill()
+        .map(()=>[]);
+
+    table[0] = [[]]
+    for(let i=0;i<=target.length;i++){
+        for(let word in wordBank){
+            if(target.slice(i, i+word.length) === word){
+                const newCombinations = table[i].map(subArray => [...subArray, word])
+                table[i+word.length].push(...newCombinations)
+                console.log(table)
+            }
+        }
+    }
     return table[target.length]
 }
 
-console.log(allConstruct("purple", ['purp', 'p', 'ur', 'le', 'purpl']))
-console.log(allConstruct("abcdef", ['ab', 'abc', 'cd', 'def', 'abcd']))
-console.log(allConstruct("skateboard", ['bo', 'rd', 'ate', 't', 'ska', 'sk', 'bar']))
-console.log(allConstruct("enterapotentpot", ['a', 'p', 'ent', 'enter', 'ot', 'o', 't']))
-console.log(allConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeef", [
-    'e',
-    'ee',
-    'eee',
-    'eeee',
-    'eeeee'
-]))
+console.log(allConstruct('purple', ['purp', 'p', 'ur', 'le', 'purpl']))
+// console.log(allConstruct("abcdef", ['ab', 'abc', 'cd', 'def', 'abcd']))
+// console.log(allConstruct("skateboard", ['bo', 'rd', 'ate', 't', 'ska', 'sk', 'bar']))
+// console.log(allConstruct("enterapotentpot", ['a', 'p', 'ent', 'enter', 'ot', 'o', 't']))
+// console.log(allConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeef", [
+//     'e',
+//     'ee',
+//     'eee',
+//     'eeee',
+//     'eeeee'
+// ]))
