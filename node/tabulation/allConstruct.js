7a4781e3d2e086d52f262aa0a17bd61d63ba79c8
PuneetSivananda <psivananda@chatsoft.com>
2022-03-20 23:18:15 +0530

    fix: tabulation submission

--- node/tabulation/allConstruct.js
+++ node/tabulation/allConstruct.js
@@ -5,25 +5,24 @@ const allConstruct = (target, wordBank)=>{
 
     table[0] = [[]]
     for(let i=0;i<=target.length;i++){
-        for(let word in wordBank){
+        for(let word of wordBank){
             if(target.slice(i, i+word.length) === word){
                 const newCombinations = table[i].map(subArray => [...subArray, word])
                 table[i+word.length].push(...newCombinations)
-                console.log(table)
             }
         }
     }
     return table[target.length]
 }
 
-console.log(allConstruct('purple', ['purp', 'p', 'ur', 'le', 'purpl']))
-// console.log(allConstruct("abcdef", ['ab', 'abc', 'cd', 'def', 'abcd']))
-// console.log(allConstruct("skateboard", ['bo', 'rd', 'ate', 't', 'ska', 'sk', 'bar']))
-// console.log(allConstruct("enterapotentpot", ['a', 'p', 'ent', 'enter', 'ot', 'o', 't']))
+console.log(allConstruct("purple", ['purp', 'p', 'ur', 'le', 'purpl']))
+console.log(allConstruct("abcdef", ['ab', 'abc', 'cd', 'def', 'abcd']))
+console.log(allConstruct("skateboard", ['bo', 'rd', 'ate', 't', 'ska', 'sk', 'bar']))
+console.log(allConstruct("enterapotentpot", ['a', 'p', 'ent', 'enter', 'ot', 'o', 't']))
 // console.log(allConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeef", [
 //     'e',
 //     'ee',
 //     'eee',
 //     'eeee',
-//     'eeeee'
+//     'eeeeef'
 // ]))
