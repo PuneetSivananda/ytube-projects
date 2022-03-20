47e9191ba9ce5b71dbc5b8a2b3d095cd17235c71
PuneetSivananda <psivananda@chatsoft.com>
2022-03-20 20:00:17 +0530

    wip: setting up binary file io

---
+++ cli-toolset/convertBinary.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"encoding/hex"
+	"fmt"
+	"log"
+)
+
+func main() {
+	content := []byte("Go is an open source programming language.")
+
+	dest := make([]byte, hex.EncodedLen(len(content)))
+	hex.Encode(dest, content)
+
+	fmt.Printf("%s", dest)
+	fmt.Println()
+	src := []byte("476f20697320616e206f70656e20736f757263652070726f6772616d6d696e67206c616e67756167652e")
+
+	dst := make([]byte, hex.DecodedLen(len(src)))
+	n, err := hex.Decode(dst, src)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%s\n", dst[:n])
+
+}
