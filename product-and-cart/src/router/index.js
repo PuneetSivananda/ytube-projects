d2cf1021c5d701000ed36f1206d6af373ece4631
PuneetSivananda <psivananda@chatsoft.com>
2022-03-22 17:43:41 +0530

    feat: migrate same logic for functionality

--- product-and-cart/src/router/index.js
+++ product-and-cart/src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import Products from '../views/ProductView.vue'
-
+import PastOrders from '../views/PastOrders.vue'
 const routes = [
   {
     path: '/',
@@ -15,6 +15,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: Products
+  },
+  {
+    path: '/past-orders',
+    name: 'PastOrders',
+    component: PastOrders
   }
 ]
 
