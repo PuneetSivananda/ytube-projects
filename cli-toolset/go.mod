7a9a983417a1d82ae178e4e757571e5ffa4522af
PuneetSivananda <psivananda@chatsoft.com>
2022-03-23 22:18:32 +0530

    feat: explore package

--- cli-toolset/go.mod
+++ cli-toolset/go.mod
@@ -1,3 +1,8 @@
 module cli-toolset
 
 go 1.17
+
+require (
+	github.com/brentp/irelate v0.0.1 // indirect
+	github.com/brentp/vcfgo v0.0.0-20190824021612-654ed2e5945d // indirect
+)
--- cli-toolset/vcf/vcf.go
+++ cli-toolset/vcf/vcf.go
@@ -1,7 +1,36 @@
 package vcf
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/brentp/vcfgo"
+)
+
 // Vcf reader functionalities
 // Parse vcf and get details either for the whole file or part of a file after selecting a subset of matching data
 func main() {
-
+	f, _ := os.Open("examples/test.auto_dom.no_parents.vcf")
+	rdr, err := vcfgo.NewReader(f, false)
+	if err != nil {
+		panic(err)
+	}
+	for {
+		variant := rdr.Read()
+		if variant == nil {
+			break
+		}
+		fmt.Printf("%s\t%d\t%s\t%v\n", variant.Chromosome, variant.Pos, variant.Ref(), variant.Alt())
+		dp, err := variant.Info().Get("DP")
+		fmt.Printf("depth: %v\n", dp.(int))
+		sample := variant.Samples[0]
+		// we can get the PL field as a list (-1 is default in case of missing value)
+		PL, err := variant.GetGenotypeField(sample, "PL", -1)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("%v\n", PL)
+		_ = sample.DP
+	}
+	fmt.Fprintln(os.Stderr, rdr.Error())
 }
