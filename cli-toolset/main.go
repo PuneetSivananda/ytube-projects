eb33467ed3753597ee4f67be611e174718de789f
PuneetSivananda <psivananda@chatsoft.com>
2022-03-23 21:57:12 +0530

    chore: updates

--- cli-toolset/main.go
+++ cli-toolset/main.go
@@ -6,23 +6,25 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 )
 
 func main() {
-	flag.Usage = func() { // [1]
-		fmt.Fprintf(flag.CommandLine.Output(), "usage: %s cmd1|cmd2\n", os.Args[0])
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(), "usage: %s -chr [no] -start [pos] -end [pos]\n", os.Args[0])
 		flag.PrintDefaults()
 	}
+	age := flag.Int("age", 0, "age of the person")
+	place := flag.String("place", "", "Place a person is born")
 	c := &colors.Colors{Clr: colors.ColorGreen}
 	flag.Parse()
 
+	//Check flgs here:
 	if len(os.Args) < 2 {
 		log.Fatalf("error: wrong number of arguments")
 	}
-
-	switch os.Args[1] {
-	case "run":
-		c.Colorize("here")
-		return
-	}
+	//build options list here:
+	strAge := strconv.Itoa(*age)
+	c.Colorize(*place + strAge)
+	//Start flow here
 }
