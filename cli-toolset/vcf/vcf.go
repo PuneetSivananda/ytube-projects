4ea74312ced2a62bd0edcd6ce186f43ae6ffba93
PuneetSivananda <psivananda@chatsoft.com>
2022-03-24 23:45:15 +0530

    chore: vcf files read sample vcf file

--- cli-toolset/vcf/vcf.go
+++ cli-toolset/vcf/vcf.go
@@ -1,4 +1,4 @@
-package vcf
+package main
 
 import (
 	"fmt"
@@ -10,7 +10,7 @@ import (
 // Vcf reader functionalities
 // Parse vcf and get details either for the whole file or part of a file after selecting a subset of matching data
 func main() {
-	f, _ := os.Open("examples/test.auto_dom.no_parents.vcf")
+	f, _ := os.Open("../data/testVcf/sample.vcf")
 	rdr, err := vcfgo.NewReader(f, false)
 	if err != nil {
 		panic(err)
@@ -20,17 +20,21 @@ func main() {
 		if variant == nil {
 			break
 		}
-		fmt.Printf("%s\t%d\t%s\t%v\n", variant.Chromosome, variant.Pos, variant.Ref(), variant.Alt())
-		dp, err := variant.Info().Get("DP")
-		fmt.Printf("depth: %v\n", dp.(int))
-		sample := variant.Samples[0]
-		// we can get the PL field as a list (-1 is default in case of missing value)
-		PL, err := variant.GetGenotypeField(sample, "PL", -1)
-		if err != nil {
-			panic(err)
-		}
-		fmt.Printf("%v\n", PL)
-		_ = sample.DP
+
+		fmt.Printf("%s\t%d\t%s\t%v", variant.Chromosome, variant.Pos, variant.Ref(), variant.Alt())
+		alleleID, _ := variant.Info().Get("ALLELEID")
+		CLNDISDB, _ := variant.Info().Get("CLNDISDB")
+		CLNDN, _ := variant.Info().Get("CLNDN")
+		CLNHGVS, _ := variant.Info().Get("CLNHGVS")
+		CLNREVSTAT, _ := variant.Info().Get("CLNREVSTAT")
+		CLNSIG, _ := variant.Info().Get("CLNSIG")
+		CLNVC, _ := variant.Info().Get("CLNVC")
+		CLNVCSO, _ := variant.Info().Get("CLNVCSO")
+		GENEINFO, _ := variant.Info().Get("GENEINFO")
+		MC, _ := variant.Info().Get("MC")
+		ORIGIN, _ := variant.Info().Get("ORIGIN")
+		RS, _ := variant.Info().Get("RS")
+		fmt.Printf("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%v\t", alleleID, CLNDISDB, CLNDN, CLNHGVS, CLNREVSTAT, CLNSIG, CLNVC, CLNVCSO, GENEINFO, MC, ORIGIN, RS)
 	}
 	fmt.Fprintln(os.Stderr, rdr.Error())
 }
