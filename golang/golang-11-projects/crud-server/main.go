35bd087f1ca5203b54bf1b6a19648408cdc7b0a3
PuneetSivananda <psivananda@chatsoft.com>
2022-04-02 22:53:14 +0530

    feat: create the skeleton and get movies func

--- golang/golang-11-projects/crud-server/main.go
+++ golang/golang-11-projects/crud-server/main.go
@@ -1,7 +1,67 @@
-package crudserver
+package main
 
-import "fmt"
+/**
+Using slices to create a crud server.
+**/
+
+import ( // Dependencies
+
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Movie struct {
+	ID       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct {
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+}
+
+var movies []Movie
+
+func getMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w)
+}
 
 func main() {
-	fmt.Println("HW")
+	r := mux.NewRouter()
+
+	movies = append(movies, Movie{
+		ID:    "1",
+		Isbn:  "428227",
+		Title: "Movie1",
+		Director: &Director{
+			FirstName: "John",
+			LastName:  "Doe",
+		},
+	})
+
+	movies = append(movies, Movie{
+		ID:    "2",
+		Isbn:  "454535",
+		Title: "Movie Two",
+		Director: &Director{
+			FirstName: "Jane",
+			LastName:  "Doe",
+		},
+	})
+
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Printf("Starting Server at Port 8000\n ")
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
