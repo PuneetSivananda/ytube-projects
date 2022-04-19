08b2f8a8c7a53f3ee6259e807eb6d647b195403a
PuneetSivananda <psivananda@chatsoft.com>
2022-04-19 21:23:39 +0530

    feat: utls package added

--- golang/golang-11-projects/bookstore-management/pkg/config/app.go
+++ golang/golang-11-projects/bookstore-management/pkg/config/app.go
@@ -1,6 +1,6 @@
 package config
 
-import(
+import (
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
@@ -9,14 +9,14 @@ var (
 	db *gorm.DB
 )
 
-func Connect(){
-	d, err := gorm.Open("mysql", "user:passwd@/simplerest?")
-	if err!= nil{
+func Connect() {
+	d, err := gorm.Open("mysql", "user:passwd@/simplerest?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
 		panic(err)
 	}
 	db = d
 }
 
-func GetDb() *gorm.DB{
-	retuen db
+func GetDB() *gorm.DB {
+	return db
 }
