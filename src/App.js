d0f961ee59c1ba97ed8e457bee6ea00c07809b46
PuneetSivananda <puneet.sivananda@gmail.com>
2022-08-20 19:57:02 +0530

    feat: baics of material ui

--- src/App.js
+++ src/App.js
@@ -1,17 +1,50 @@
-import { Button } from "@mui/material"
+import { Button, styled, Typography } from "@mui/material"
 import { Add, Settings } from "@mui/icons-material";
+import { theme } from "./theme";
 
 function App() {
+  const BlueButton = styled(Button)(({ theme }) => ({
+    backgroundColor: theme.palette.otherColor.main,
+    color: "black",
+    margin: 5,
+    "&:hover": {
+      backgroundColor: "lightblue"
+    },
+    "&:disabled": {
+      backgroundColor: "gray",
+      color: "white"
+    }
+  }))
   return (
     <div>
       <Button variant="text">Text</Button>
-      <Button startIcon={<Settings />} variant="contained" color="secondary" size="small">
+      <Button startIcon={<Settings />} variant="contained" color="otherColor" size="small">
         Settings
       </Button>
       <Button startIcon={<Add />} variant="contained" color="success" size="small">
         Add new post
       </Button>
       <Button variant="outlined" disabled>Outlined</Button>
+
+      <Typography variant="h1" component={"h2"}>
+        h1.Heading
+      </Typography>
+      <Button variant="contained" disabled sx={{
+        backgroundColor: "skyblue",
+        color: "#888",
+        margin: 5,
+        "&:hover": {
+          backgroundColor: "lightblue"
+        },
+        "&:disabled": {
+          backgroundColor: "gray",
+          color: "white"
+        }
+      }}>
+        Another Button
+      </Button>
+      <BlueButton>MyButton</BlueButton>
+      <BlueButton>One More MyButton</BlueButton>
     </div>
   );
 }
