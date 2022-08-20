1d889a3ea76dbf8ed88d3164003dfb948a57850d
PuneetSivananda <puneet.sivananda@gmail.com>
2022-08-20 19:29:40 +0530

    feat: learn mui buttons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { Button } from "@mui/material"
+import { Add, Settings } from "@mui/icons-material";
+
 function App() {
   return (
     <div>
-      Hello World 
+      <Button variant="text">Text</Button>
+      <Button startIcon={<Settings />} variant="contained" color="secondary" size="small">
+        Settings
+      </Button>
+      <Button startIcon={<Add />} variant="contained" color="success" size="small">
+        Add new post
+      </Button>
+      <Button variant="outlined" disabled>Outlined</Button>
     </div>
   );
 }
