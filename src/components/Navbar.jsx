45e46b69206456ad5323a675a42db66f3bc72183
PuneetSivananda <puneet.sivananda@gmail.com>
2022-08-21 13:05:25 +0530

    feat: added styled navbar with breakpoints

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,32 @@
-import { Box } from "@mui/material";
+import { Pets } from "@mui/icons-material";
+import { AppBar, Box, styled, Toolbar, Typography } from "@mui/material";
 import React from "react";
+
+const StyledToolbar = styled(Toolbar)({
+    display: "flex",
+    justifyContent: "space-between"
+})
 
 const Navbar = () => {
     return (
-        <Box>Navbar</Box>
+        <AppBar position="sticky">
+            <StyledToolbar>
+                <Typography variant="h6" sx={{
+                    display: {
+                        xs: "none",
+                        sm: "block"
+                    }
+                }}>
+                    LAMA DEV
+                </Typography>
+                <Pets sx={{
+                    display: {
+                        xs: "block",
+                        sm: "none"
+                    }
+                }} />
+            </StyledToolbar>
+        </AppBar>
     )
 }
 
