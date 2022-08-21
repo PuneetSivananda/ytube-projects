ac995dc0017762e3003bb44854f534f5463234f1
PuneetSivananda <puneet.sivananda@gmail.com>
2022-08-21 13:24:41 +0530

    feat: added search bar to the toolbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,23 @@
 import { Pets } from "@mui/icons-material";
-import { AppBar, Box, styled, Toolbar, Typography } from "@mui/material";
+import { AppBar, Box, InputBase, styled, Toolbar, Typography } from "@mui/material";
 import React from "react";
 
 const StyledToolbar = styled(Toolbar)({
     display: "flex",
     justifyContent: "space-between"
 })
+
+const Search = styled("div")(({ theme }) => ({
+    backgroundColor: "white",
+    padding: "0 10px",
+    borderRadius: theme.shape.borderRadius,
+    width: "40%"
+}))
+
+const Icons = styled(Box)(({ theme }) => ({
+    backgroundColor: "lightgray"
+}))
+
 
 const Navbar = () => {
     return (
@@ -25,6 +37,12 @@ const Navbar = () => {
                         sm: "none"
                     }
                 }} />
+                <Search>
+                    <InputBase placeholder="Search..." />
+                </Search>
+                <Icons>
+                    Icons
+                </Icons>
             </StyledToolbar>
         </AppBar>
     )
