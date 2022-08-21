e52df25b6afeeba1c4d004a50b36314d7223c516
PuneetSivananda <puneet.sivananda@gmail.com>
2022-08-21 19:27:45 +0530

    feat: completed sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,14 +1,80 @@
 import React from "react";
-import { Box } from "@mui/material";
+import { Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Switch } from "@mui/material";
+import { AccountBox, Article, Group, Home, Person, Settings, ModeNight, Storefront } from "@mui/icons-material";
 
 const Sidebar = () => {
     return (
-        <Box bgcolor={"skyblue"}
+        <Box
             flex={1}
             p={2}
             sx={{ display: { xs: "none", sm: "block" } }}
         >
-            Sidebar
+            <List>
+                <ListItem disablePadding>
+                    <ListItemButton component="a" href="#">
+                        <ListItemIcon>
+                            <Home />
+                        </ListItemIcon>
+                        <ListItemText primary="HomePage" />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                    <ListItemButton component="a" href="#">
+                        <ListItemIcon>
+                            <Article />
+                        </ListItemIcon>
+                        <ListItemText primary="Pages" />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                    <ListItemButton component="a" href="#">
+                        <ListItemIcon>
+                            <Group />
+                        </ListItemIcon>
+                        <ListItemText primary="Groups" />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                    <ListItemButton component="a" href="#">
+                        <ListItemIcon>
+                            <Storefront />
+                        </ListItemIcon>
+                        <ListItemText primary="MarketPlace" />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                    <ListItemButton component="a" href="#">
+                        <ListItemIcon>
+                            <Person />
+                        </ListItemIcon>
+                        <ListItemText primary="Users" />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                    <ListItemButton component="a" href="#">
+                        <ListItemIcon>
+                            <Settings />
+                        </ListItemIcon>
+                        <ListItemText primary="Settings" />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                    <ListItemButton component="a" href="#">
+                        <ListItemIcon>
+                            <AccountBox />
+                        </ListItemIcon>
+                        <ListItemText primary="Profile" />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                    <ListItemButton component="a" href="#">
+                        <ListItemIcon>
+                            <ModeNight />
+                        </ListItemIcon>
+                        <Switch />
+                    </ListItemButton>
+                </ListItem>
+            </List>
         </Box>
     )
 }
