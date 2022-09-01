de9e9df8018cd21e7ed58d5953b4670e7b52b246
PuneetSivananda <puneet.sivananda@gmail.com>
2022-09-01 18:14:11 +0530

    feat: mapbox integration done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,20 @@
+import Map from 'react-map-gl';
+import { useState } from "react"
 
 function App() {
+  const [viewport, setViewport] = useState({
+    latitude: 37.7577,
+    longitude: -122.4376,
+    zoom: 8
+  })
   return (
     <div>
-      Hello World!
+      <Map
+        style={{ width: "100vw", height: "100vh" }}
+        initialViewState={viewport}
+        mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+        mapStyle="mapbox://styles/mapbox/dark-v10"
+      />
     </div>
   );
 }
