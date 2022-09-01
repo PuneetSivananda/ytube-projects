062fe906f2899ccc1d1903302339fb747bbcc370
PuneetSivananda <puneet.sivananda@gmail.com>
2022-09-01 22:12:51 +0530

    fix: css issue for the map layer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,28 @@
-import Map from 'react-map-gl';
+import Map, { Marker } from 'react-map-gl';
 import { useState } from "react"
+import { Room } from "@material-ui/icons"
 
 function App() {
   const [viewport, setViewport] = useState({
-    latitude: 37.7577,
-    longitude: -122.4376,
-    zoom: 8
+    latitude: 46,
+    longitude: 17,
+    zoom: 4
   })
-  return (
-    <div>
-      <Map
-        style={{ width: "100vw", height: "100vh" }}
-        initialViewState={viewport}
-        mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-        mapStyle="mapbox://styles/mapbox/dark-v10"
-      />
-    </div>
+  return (<Map
+    style={{ width: "100vw", height: "100vh" }}
+    initialViewState={viewport}
+    mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+    mapStyle="mapbox://styles/mapbox/streets-v9"
+  >
+    <Marker
+      latitude={48.8584}
+      longitude={2.2945}
+      offsetLeft={-20}
+      offsetRight={-10}
+    >
+      <Room />
+    </Marker>
+  </Map>
   );
 }
 
