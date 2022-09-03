09b1ef62da385ad27a1d4f1d8fc389e986cd4ddc
PuneetSivananda <puneet.sivananda@gmail.com>
2022-09-03 21:15:05 +0530

    feat: created the ui for the popup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
-import Map, { Marker } from 'react-map-gl';
+import Map, { Marker, Popup } from 'react-map-gl';
 import { useState } from "react"
-import { Room } from "@material-ui/icons"
+import { Room, Star } from "@material-ui/icons"
 
 function App() {
   const [viewport, setViewport] = useState({
@@ -8,6 +8,7 @@ function App() {
     longitude: 17,
     zoom: 4
   })
+  const [showPopup, setShowPopup] = useState(true)
   return (<Map
     onMove={(nextViewport) => setViewport(nextViewport)}
     style={{ width: "100vw", height: "100vh" }}
@@ -27,7 +28,37 @@ function App() {
         color: "slateblue"
       }} />
     </Marker>
-  </Map>
+    {/* {showPopup && ( */}
+    <Popup
+      latitude={48.8584}
+      longitude={2.2945}
+      anchor="left"
+    // onClose={() => setShowPopup(false)}
+    >
+      <div className='card'>
+        <label>Place:</label>
+        <h4 className='place'>Eiffel Tower</h4>
+        <label>Review:</label>
+        <p>Beautiful Place, I liked it!</p>
+        <label>Rating:</label>
+        <div className='stars'>
+          <Star />
+          <Star />
+          <Star />
+          <Star />
+          <Star />
+        </div>
+        <label>Information: </label>
+        <span className='username'>
+          Created by <b>Puneet</b>
+        </span>
+        <span className='date'>
+          1 Hour Ago
+        </span>
+      </div>
+    </Popup>)
+    {/* } */}
+  </Map >
   );
 }
 
