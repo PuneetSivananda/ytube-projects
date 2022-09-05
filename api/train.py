22401845f1ed7cea6c57c3863ea4bf2c051f6976
PuneetSivananda <puneet.sivananda@gmail.com>
2022-09-05 22:41:18 +0530

    feat: training function for encoding the data

--- api/train.py
+++ api/train.py
@@ -1,5 +1,6 @@
 import json
 from nltk_utils import tokenize, stem, bag_of_words
+import numpy as np
 
 with open("intents.json", "r") as f:
     intents = json.load(f)
@@ -24,5 +25,21 @@ ignore_words = [
     ","
 ]
 
-print(all_words)
+# stemming
+all_words = [stem(w) for w in all_words if w not in ignore_words]
+# sorting words and unique words
+all_words = sorted(set(all_words))
+tags = sorted(set(tags))
 
+X_train = []
+y_train = []
+
+for (patter_sentence, tag) in xy:
+    bag = bag_of_words(patter_sentence, all_words)
+    X_train.append(bag)
+
+    label = tags.index(tag)
+    y_train.append(label) #1-hot encoded vector needed i.e CrossEntropy Loss
+
+X_train = np.array(X_train)
+y_train = np.array(y_train)
