cfbbe805df8f52306bb7cb3bdc2c66fba6701fe8
PuneetSivananda <puneet.sivananda@gmail.com>
2022-09-05 22:49:32 +0530

    feat: implementing bag of words

--- api/nltk_utils.py
+++ api/nltk_utils.py
@@ -1,5 +1,6 @@
 import nltk
 from nltk.stem.porter import PorterStemmer
+import numpy as np
 
 stemmer = PorterStemmer()
 
@@ -10,10 +11,21 @@ def stem(word):
     return stemmer.stem(word.lower())
 
 def bag_of_words(tokenized_sentence, all_words):
-    pass
+    """
+    sentence = ["hello", "how", "are", "you"]
+    words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
+    bag = [0, 1, 0, 1, 0, 0, 0]
+    """
+    tokenized_sentence = [stem(w) for w in tokenized_sentence]
+    bag = np.zeros(len(all_words), dtype=np.float32)
+    for idx, w in enumerate(all_words):
+        if w in tokenized_sentence:
+            bag[idx] = 1.0
+    return bag
 
+sentence = ["hello", "how", "are", "you"]
+all_words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
+bag = bag_of_words(sentence, all_words)
+print(bag)
 
-# s = ['How', 'long', 'does', 'shipping', 'take', '?']
-# print(['How', 'long', 'does', 'shipping', 'take', '?'])
-# print([stem(word) for word in s])
 
