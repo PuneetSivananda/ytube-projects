22454785cf4a38b849388c3477725675a5446a1d
PuneetSivananda <puneet.sivananda@gmail.com>
2022-09-06 13:15:32 +0530

    feat: added for the pytorch mode

--- api/train.py
+++ api/train.py
@@ -6,6 +6,8 @@ import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
 
+from models import NeuralNet
+
 with open("intents.json", "r") as f:
     intents = json.load(f)
 
@@ -64,6 +66,16 @@ class ChatDataset(Dataset):
 
 # Hyper parameters
 batch_size = 8
+hidden_size = 8
+output_size = len(tags)
+input_size = len(X_train[0])
+
 
 dataset = ChatDataset()
 train_loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True, num_workers=2)
+
+# check for gpu support
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = NeuralNet(input_size, hidden_size, output_size).to(device)
+
+# loss and optimizer
