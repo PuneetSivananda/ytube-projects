24513283421ed4355dd403140824a38384e7b294
PuneetSivananda <puneet.sivananda@gmail.com>
2022-09-06 12:45:11 +0530

    feat: data modelling the dataset

--- api/train.py
+++ api/train.py
@@ -47,3 +47,23 @@ for (patter_sentence, tag) in xy:
 
 X_train = np.array(X_train)
 y_train = np.array(y_train)
+
+
+class ChatDataset(Dataset):
+    def __init__(self):
+        self.n_samples = len(X_train)
+        self.x_data = X_train
+        self.y_data = y_train
+
+    def __getitem__(self, idx):
+        return self.x_data[idx], self.y_data[idx]
+    
+    def __len__(self):
+        return self.n_samples
+
+
+# Hyper parameters
+batch_size = 8
+
+dataset = ChatDataset()
+train_loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True, num_workers=2)
