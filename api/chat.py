b874f1e43e11950a360e458423c0cb0b2fba4d12
PuneetSivananda <puneet.sivananda@gmail.com>
2022-09-08 12:15:59 +0530

    feat: refactor chat bot

--- api/chat.py
+++ api/chat.py
@@ -31,13 +31,9 @@ model.eval()
 # Chat logic 
 
 bot_name = "Sam"
-print("Let's chat! type 'quit' to exit")
-while True:
-    sentence = input("You: ")
-    if sentence == "quit":
-        break
-    
-    sentence = tokenize(sentence)
+
+def get_response(msg):
+    sentence = tokenize(msg)
     X = bag_of_words(sentence, all_words)
     X = X.reshape(1, X.shape[0])
     X = torch.from_numpy(X)
@@ -52,9 +48,18 @@ while True:
     if prob.item()> 0.75:
         for intent in intents["intents"]:
             if tag == intent["tag"]:
-                print(f"{bot_name}: {random.choice(intent['responses'])}")
-    else:
-        print(f"{bot_name}: I do not understand!...")
+                return random.choice(intent['responses'])
+    return "I do not understand!..."
 
+
+if __name__ == "__main__":
+    print("Let's chat! type 'quit' to exit")
+    while True:
+        sentence = input("You: ")
+        if sentence == "quit":
+            break
+        resp = get_response(sentence)
+        print(resp)
+    
 
 
