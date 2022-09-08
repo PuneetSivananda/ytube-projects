c7872d33deff20bf4c6fdaea975d76990d7b2996
PuneetSivananda <puneet.sivananda@gmail.com>
2022-09-08 18:41:01 +0530

    feaT: implement flask skeleton

---
+++ api/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request, jsonify
+
+from chat import get_response
+
+app = Flask(__name__)
+
+@app.get("/")
+def index_get():
+    return render_template("base.html")
+
+@app.post("/predict")
+def predict():
+    pass
+    
