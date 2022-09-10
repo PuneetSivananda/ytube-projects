95c34b43dca2a2409ae50c16db04d6dfa5a6bd7f
PuneetSivananda <puneet.sivananda@gmail.com>
2022-09-11 00:27:44 +0530

    chore: chatbot updates

---
+++ api/static/app.js
@@ -0,0 +1,11 @@
+class Chatbot {
+    constructor(){
+        this.args = {
+            openButton: document.querySelector('.chatbox__button'),
+            chatBox: document.querySelector('.chatbox__support'),
+            sendButton: document.querySelector('.send__button')
+        }
+        this.state = false;
+        this.messages = []
+    }
+}
