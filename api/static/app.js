3759a7b14b26679d9d239f03f9e1b739151b0270
PuneetSivananda <puneet.sivananda@gmail.com>
2022-09-12 22:57:44 +0530

    feat: updates to the front end module for controlling the user actions

--- api/static/app.js
+++ api/static/app.js
@@ -1,5 +1,5 @@
 class Chatbot {
-    constructor(){
+    constructor() {
         this.args = {
             openButton: document.querySelector('.chatbox__button'),
             chatBox: document.querySelector('.chatbox__support'),
@@ -7,5 +7,57 @@ class Chatbot {
         }
         this.state = false;
         this.messages = []
+    }
+    display() {
+        const { openButton, chatBox, sendButton } = this.args
+        openButton.addEventListener('click', () => this.toggleState(chatBox))
+        sendButton.addEventListener('click', () => this.onSendButton(chatBox))
+
+        const node = chatBox.querySelector('input')
+        node.addEventListener("keyup", ({ key }) => {
+            if (key === "Enter") {
+                this.onSendButton(chatBox)
+            }
+        })
+    }
+
+    toggleState(chatbox) {
+        this.state = !this.state;
+
+        if (this.state) {
+            chatbox.classList.add('chatbot--active')
+        } else {
+            chatbox.classList.remove('chatbot--active')
+        }
+    }
+
+    onSendButton(chatbox) {
+        var textField = chatbox.querySelector('input')
+        let text1 = textField.value
+        if (text1 == "") {
+            return;
+        }
+        let msg1 = { name: "User", message: text1 }
+        this.messages.push(msg1)
+
+        fetch($SCRIPT_ROOT + "/predict", {
+            method: "POST",
+            body: JSON.stringify({ message: text1 }),
+            mode: "cors",
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(r => r.json())
+            .then(r => {
+                let msg2 = { name: "Sam", message: r.answer }
+                this.messages.push(msg2)
+                this.updateChatText(chatbox)
+                textField.value = ""
+            }).catch(err => {
+                console.error('Error:', err)
+                this.updateChatText(chatbox)
+                textField.value = ""
+            })
     }
 }
