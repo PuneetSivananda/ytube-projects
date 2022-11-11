0b3baa574aaba91044a10bf31b07f1973f0dbff6
Puneet Sivananda <puneet.sivananda@tensoriot.com>
2022-11-11 16:15:37 +0530

    feat: encoding the data before classification

--- 2.Classification/KNN_Classification.py
+++ 2.Classification/KNN_Classification.py
@@ -2,9 +2,31 @@ import sklearn
 from sklearn.utils import shuffle
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import linear_model, preprocessing
+from sklearn.model_selection import train_test_split
 import pandas as pd
 import numpy as np
 
 
 data = pd.read_csv("../0.data/car.data")
 print(data.head())
+
+# preprocessing the data using label encoder
+
+le = preprocessing.LabelEncoder()
+buying = le.fit_transform(list(data["buying"]))
+maint = le.fit_transform(list(data["maint"]))
+door = le.fit_transform(list(data["door"]))
+persons = le.fit_transform(list(data["persons"]))
+lug_boot = le.fit_transform(list(data["lug_boot"]))
+safety = le.fit_transform(list(data["safety"]))
+cls = le.fit_transform(list(data["class"]))
+
+# encoded the data
+
+predict = "class"
+X= list(zip(buying, maint, door, persons, lug_boot, safety))
+y= list(cls)
+
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
+
+print(x_train, y_test)
