1bd90ed13093d05aa498519ef7ad76890ee54962
PuneetSivananda <puneet.sivananda@gmail.com>
2022-11-13 20:02:29 +0530

    feat: svm algorithm implementation

--- 3.SVM/SVM_Algorithm.py
+++ 3.SVM/SVM_Algorithm.py
@@ -1,12 +1,12 @@
 import sklearn
-from sklearn import datasets
-from sklearn import svm
+from sklearn import datasets, metrics
+from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
 
 cancer = datasets.load_breast_cancer()
 
-print(cancer.feature_names)
-print(cancer.target_names)
+# print(cancer.feature_names)
+# print(cancer.target_names)
 
 X = cancer.data
 y = cancer.target
@@ -14,4 +14,17 @@ classes = ['malignant', 'benign']
 
 x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-print(x_train, y_train)
+# print(x_train, y_train)
+# SVM classification 
+clf = SVC(
+    kernel="linear",
+    C=2
+    # kernel="poly",
+    # degree=2
+)
+clf.fit(x_train, y_train)
+
+y_pred = clf.predict(x_test)
+acc = metrics.accuracy_score(y_test, y_pred)
+print(acc)
+
