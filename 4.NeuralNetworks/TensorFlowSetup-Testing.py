27a40fb60f47dae8537e549d2f6942ff6e1d0517
Puneet Sivananda <puneet.sivananda@tensoriot.com>
2022-11-14 12:50:52 +0530

    feat: get the image to be displayed

--- 4.NeuralNetworks/TensorFlowSetup-Testing.py
+++ 4.NeuralNetworks/TensorFlowSetup-Testing.py
@@ -3,8 +3,20 @@ from tensorflow import keras
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = keras.datasets.fashion_mnist
 
+print(tf.__version__)
+
+data = keras.datasets.fashion_mnist
 (train_images, train_labels),(test_images, test_labels) = data.load_data()
 
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
 print(train_labels[6])
+
+
+plt.figure()
+plt.imshow(train_images[6])
+plt.colorbar()
+plt.grid(False)
+plt.show()
