3f2fc0f021922852616535c3b0d2599d98d55bd9
Puneet Sivananda <puneet.sivananda@tensoriot.com>
2022-11-14 16:20:45 +0530

    feat: unable to get the datasets

--- 4.NeuralNetworks/TensorFlowSetup-Testing.py
+++ 4.NeuralNetworks/TensorFlowSetup-Testing.py
@@ -23,3 +23,19 @@ plt.imshow(train_images[6])
 plt.colorbar()
 plt.grid(False)
 plt.show()
+
+# Training Steps
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation="relu"),
+    keras.layers.Dense(10, activation="softmax"),
+])
+
+model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+model.fit(train_images, train_labels, epochs=5)
+
+# Testing the model
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+print("Tested Accuracy: ", test_acc)
+
+
