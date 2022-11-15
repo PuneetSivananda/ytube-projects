6d00fc597ca973caf84cc47453ca2488e1bd64bc
PuneetSivananda <puneet.sivananda@gmail.com>
2022-11-15 22:55:15 +0530

    feat: building the keras model

--- 4.NeuralNetworks/Tensorflow_keras_classification.py
+++ 4.NeuralNetworks/Tensorflow_keras_classification.py
@@ -5,4 +5,37 @@ import numpy as np
 
 data = keras.datasets.imdb
 
-(train_images, train_labels),(test_images, test_labels) = data.load_data()
+(train_data, train_labels),(test_data, test_labels) = data.load_data(num_words=10000)
+
+# print(test_data[0])
+
+word_index = data.get_word_index()
+
+word_index = {k:(v+3) for k,v in word_index.items()}
+word_index["<PAD>"] = 0
+word_index["<START>"] = 1
+word_index["<UNK>"] = 2
+word_index["<UNUSED>"] = 3
+
+# point words to keys 
+reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
+
+# fancy tensorflow functions
+train_data = keras.preprocessing.sequence.pad_sequences(train_data, value=word_index["<PAD>"], padding="post", maxlen=250)
+test_data = keras.preprocessing.sequence.pad_sequences(test_data, value=word_index["<PAD>"], padding="post", maxlen=250)
+
+def decode_review(text):
+    return " ".join([reverse_word_index.get(i, "?") for i in text])
+
+# print(decode_review(test_data[0]))
+# print(len(test_data[0]), len(test_data[2]))
+
+# model building
+
+model = keras.Sequential()
+model.add(keras.layers.Embedding(10000, 16))
+model.add(keras.layers.GlobalAveragePooling1D())
+model.add(keras.layers.Dense(16, activation="relu"))
+model.add(keras.layers.Dense(1, activation="sigmoid"))
+
+
