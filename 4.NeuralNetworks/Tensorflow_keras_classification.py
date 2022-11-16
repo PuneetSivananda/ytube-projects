56f8f696fb16b0419f39a61e053109fe6589973c
Puneet Sivananda <puneet.sivananda@tensoriot.com>
2022-11-16 12:34:33 +0530

    feat: NN model predictions

--- 4.NeuralNetworks/Tensorflow_keras_classification.py
+++ 4.NeuralNetworks/Tensorflow_keras_classification.py
@@ -38,4 +38,30 @@ model.add(keras.layers.GlobalAveragePooling1D())
 model.add(keras.layers.Dense(16, activation="relu"))
 model.add(keras.layers.Dense(1, activation="sigmoid"))
 
+# Model training
+model.summary()
 
+model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
+
+x_val = train_data[:10000]
+x_train = train_data[10000:]
+
+y_val = train_labels[:10000]
+y_train = train_labels[10000:] 
+
+fitModel = model.fit(x_train, y_train, epochs=40, batch_size=512, validation_data=(x_val, y_val), verbose=1)
+results = model.evaluate(test_data, test_labels)
+
+print(results)
+print("Loss: ", results[0])
+print("Accuracy: ", results[1])
+
+# Model Predictions
+test_review = test_data[0]
+print(test_review)
+print("Review: ")
+print(decode_review(test_review))
+
+predict = model.predict(np.array([test_review]))
+print("Prediction: "+str(predict[0]))
+print("Actual: "+ str(test_labels[0]))
