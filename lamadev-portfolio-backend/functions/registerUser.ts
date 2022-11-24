6722d794ce0f1399af0f75740f4b68dc83a0ebe5
PuneetSivananda <puneet.sivananda@gmail.com>
2022-11-25 01:41:17 +0530

    figure out content type

--- lamadev-portfolio-backend/functions/registerUser.ts
+++ lamadev-portfolio-backend/functions/registerUser.ts
@@ -3,44 +3,41 @@ import { connectToDatabase } from "./db"
 import Users from "./models/Users"
 
 const handler: Handler = async (event: HandlerEvent, context, callback: any) => {
-    context.callbackWaitsForEmptyEventLoop = false;
     let user: {
         username: string,
         password: string,
         email: string
     }
-    
+    console.log(event)
+    console.log(event.httpMethod)
     if (event.body) {
-        user = JSON.parse(event.body)
-        console.log(user)
-        // if correct write to db
+        console.log(JSON.parse(event.body))
+        // user = JSON.parse(event.body)
+        // // if correct write to db
+        // console.log(user.username)
     }
 
-    connectToDatabase()
-        .then(() => {
-            Users.create(user)
-                .then(note =>
-                    callback(null, {
-                        statusCode: 200,
-                        body: JSON.stringify(user)
-                    })
-                )
-        })
-        .catch(err =>
-            callback(null, {
-                statusCode: err.statusCode || 500,
-                headers: { 'Content-Type': 'text/plain' },
-                body: 'Could not create the todoitem.'
-            })
-        );
     return {
-        statusCode: 200,
-        body: JSON.stringify({
-            'path': "home",
-            "firstName": "Bob",
-            "lastName": "Sager",
-        })
+        statusCode: 200
     }
+
+    // return connectToDatabase()
+    //     .then(() => {
+    //         Users.create(user)
+    //             .then(() =>
+    //                 callback(null, {
+    //                     statusCode: 200,
+    //                     body: JSON.stringify(user)
+    //                 })
+    //             )
+    //     })
+    //     .catch(err =>
+    //         callback(null, {
+    //             statusCode: err.statusCode || 500,
+    //             headers: { 'Content-Type': 'text/plain' },
+    //             body: 'Could not create the user.'
+    //         })
+    //     );
 }
 
 export { handler }
