8cab8340b8d82e9dc1ed6b4e3c5995777f64160c
PuneetSivananda <puneet.sivananda@gmail.com>
2022-11-27 00:11:21 +0530

    feaT: user follow flow

--- lamadev-portfolio-backend/functions/followUser.ts
+++ lamadev-portfolio-backend/functions/followUser.ts
@@ -8,37 +8,41 @@ const handler: Handler = async (event: HandlerEvent, context, callback: any) => 
     context.callbackWaitsForEmptyEventLoop = false;
 
     const { userId }: any = event.queryStringParameters
-    console.log(userId)
     const bodyUser = JSON.parse(event.body || "{}")
     let flag = false
     if (userId !== bodyUser.userId) {
         try {
-            console.log(flag)
             await connectToDatabase()
                 .then(async () => {
                     const user = await Users.findById(userId)
                     const currentUser = await Users.findById(bodyUser.userId)
                     if (!user?.followers?.includes(bodyUser.userId)) {
                         await user?.updateOne({ $push: { followers: bodyUser.userId } })
-                        await currentUser?.updateOne({ $push: { following: bodyUser.userId } })
+                        await currentUser?.updateOne({ $push: { following: userId } })
                         return
                     } else {
                         flag = true
                     }
                 })
-
+            if (flag) {
+                return {
+                    statusCode: 200,
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({ "state": "User Already followed" },)
+                }
+            }
+            return {
+                statusCode: 200,
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ "state": "User has been followed" },)
+            }
         } catch (e) {
             return errorResponse(500, JSON.stringify(e))
         }
-    } else {
+    }
+    else {
         return errorResponse(403, "You cannot follow yourself")
     }
-    return {
-        statusCode: 200,
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ "state": "User Already followed" },)
-    }
-
 }
 
 export { handler }
