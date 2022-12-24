a0a346cd90f82e01c48d1d66e37a56b4f9c0d025
PuneetSivananda <puneet.sivananda@gmail.com>
2022-12-24 20:07:13 +0530

    feat: layoures are a pain

---
+++ ninja-gaming/src/routes/about/+layout.svelte
@@ -0,0 +1,40 @@
+<script>
+	import Title from "$lib/title.svelte";
+    import "../../styles/global.css"
+</script>
+
+<header>
+    <Title title="Ninja Gaming Guides"/>
+    <div class="links">
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/help/hello">Hello</a>
+    </div>
+</header>
+<main>
+    <!-- page content -->
+    <slot></slot>
+</main>
+<footer>
+    <p>Copyright 2022 Ninja gaming guide</p>
+</footer>
+
+<style>
+    header{
+        display: flex;
+        justify-content: center;
+    }
+    main{
+        max-width: 960px;
+        margin: 20px auto;
+    }
+    footer{
+        text-align: center;
+    }
+    .links{
+        margin-left: auto;
+    }
+    a{
+        margin-left: 10px ;
+    }
+</style>
