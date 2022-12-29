e1ad5a3e09511212dc9b53eb75861507ceb2fec7
PuneetSivananda <puneet.sivananda@gmail.com>
2022-12-29 23:23:28 +0530

    feat: data analysis in js

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# ytube-projects
-git clone --single-branch --branch <branchname> <remote-repo>
+[//]: <> (Reference https://www.youtube.com/watch?v=k0mx-bNOtJA)
+
+
+- Data forge
+
+- Datasets
+- Stackover flow data: 
+    https://raw.githubusercontent.com/franciscadias/data/master/stack-overflow-data.csv
+- Irid data:
+    https://gist.githubusercontent.com/netj/8836201/raw/6f9306ad21398ea43cba4f7d537619d0e07d5ae3/iris.csv
---
+++ src/index.js
@@ -0,0 +1,26 @@
+import { DataFrame } from "data-forge";
+
+
+const df = new DataFrame(
+    [{ A: 10 }, { A: 20 }, { A: 30 }, { A: 40 }]
+)
+// console.log(df.toJSON())
+
+
+const df1 = new DataFrame({
+    columnNames: ["id", "names", "sex", "age"],
+    rows: [
+        [1, "Jesse", "F", 25],
+        [2, "Jane", "F", 35],
+        [3, "Sighn", "M", 26],
+        [4, "Ali", "M", 27],
+        [5, "Mary", "F", 15],
+    ],
+})
+
+console.log(df1)
+
+// as an array
+console.log(df1.toArray())
+console.log(df1.toRows())
+console.log(df1.toPairs())
