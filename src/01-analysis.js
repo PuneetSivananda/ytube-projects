2cd8432cd1de07d65eae0bf3f019d2c57db64081
PuneetSivananda <puneet.sivananda@gmail.com>
2022-12-30 10:55:43 +0530

    feat: updates

--- src/01-analysis.js
+++ src/01-analysis.js
@@ -1,4 +1,15 @@
 import { readFileSync } from "data-forge-fs";
 
 const df = readFileSync('data/iris.csv').parseCSV()
-console.log(df)
+console.log(df.toString())
+
+// head 
+console.log(df.head(5).toString())
+// tail
+console.log(df.tail(5).toString())
+
+// get datatypes  | describr alternative
+console.log(df.detectTypes().toString())
+
+// get values
+console.log(df.detectValues().toString())
