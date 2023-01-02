f8329c7ce45c035b136d973d839e0e1e209fb93a
PuneetSivananda <puneet.sivananda@gmail.com>
2023-01-02 22:57:56 +0530

    feat: updates for video detection

--- ml5/object-detection-coco/index.js
+++ ml5/object-detection-coco/index.js
@@ -1,5 +1,6 @@
-let img
+let video
 let detector
+let detections = []
 
 function preload() {
     img = loadImage('images/bird.jpeg')
@@ -10,22 +11,31 @@ function gotDetections(error, results) {
     if (error) {
         console.error(error)
     }
-    console.log(results)
-    for (let i = 0; i < results.length; i++) {
-        let object = results[i]
-        stroke(0, 234, 0)
-        strokeWeight(3)
-        noFill()
-        rect(object.x, object.y, object.width, object.height)
-        fill(255)
-        textSize(24)
-        text(object.label, object.x + 10, object.y + 24)
-    }
+    detections = results
+    detector.detect(video, gotDetections)
 }
 
 function setup() {
-    createCanvas(640, 480);
-    image(img, 0, 0, height, width)
-    detector.detect(img, gotDetections)
+    createCanvas(700, 500);
+    // for video capture
+    video = createCapture(VIDEO)
+    video.size(640, 480)
+    video.hide()
+    detector.detect(video, gotDetections)
 }
 
+function draw() {
+    image(video, 0, 0)
+    if (detections != undefined && detections.length > 0) {
+        for (let i = 0; i < detections.length; i++) {
+            let object = detections[i]
+            stroke(0, 234, 0)
+            strokeWeight(3)
+            noFill()
+            rect(object.x, object.y, object.width, object.height)
+            fill(255)
+            textSize(24)
+            text(object.label, object.x + 10, object.y + 24)
+        }
+    }
+}
