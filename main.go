530903d172f68e790c1a93bd67acd95b949c34c4
PuneetSivananda <puneet.sivananda@gmail.com>
2023-01-08 00:20:16 +0530

    wip: refactor the flag usage to urfi

--- main.go
+++ main.go
@@ -19,9 +19,15 @@ func main() {
 	app.Name = "MyCalc"
 	app.Usage = "A fast calc app in the terminal."
 	app.Description = "A Longer desription for a terminal calculater that can be used in the terminal."
-	app.Action = func(*cli.Context) error {
-		fmt.Println("boom! I Say!")
-		return nil
+	app.Action = mainAction
+	app.Authors = []*cli.Author{
+		{Name: "Puneet Sivananda", Email: "puneet.sivananda@gmail.com"},
+	}
+	app.Flags = []cli.Flag{
+		&cli.StringFlag{},
+		&cli.StringFlag{Destination: &oper, Name:"oper", Value: "add", Usage: "add, sub, mul, or div operation on two int operands"}
+		flag.IntVar(&op1, "op1", 0, "operand 1 for operation")
+		flag.IntVar(&op2, "op2", 0, "operand 2 for operation")
 	}
 
 	if err := app.Run(os.Args); err != nil {
@@ -29,12 +35,8 @@ func main() {
 	}
 }
 
-func oldMain() {
-	flag.StringVar(&oper, "oper", "add", "add, sub, mul, or div operation on two int operands")
-	flag.IntVar(&op1, "op1", 0, "operand 1 for operation")
-	flag.IntVar(&op2, "op2", 0, "operand 2 for operation")
+func mainAction(ctx *cli.Context) error {
 	flag.Parse()
-
 	switch oper {
 	case "add":
 		res := op1 + op2
@@ -47,10 +49,10 @@ func oldMain() {
 		fmt.Printf("%v * %v = %v\n", op1, op2, res)
 	case "div":
 		if op2 == 0 {
-			fmt.Printf("op2 is equal to 0, can't divid by 0")
-			return
+			return fmt.Errorf("op2 is equal to 0, can't divid by 0")
 		}
 		res := op1 / op2
 		fmt.Printf("%v / %v = %v\n", op1, op2, res)
 	}
+	return nil
 }
