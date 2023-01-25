01e83c1737b77f8ec7098c8be1bdfc7dbc037bc6
Puneet Sivananda <puneet.sivananda@tensoriot.com>
2023-01-25 12:56:29 +0530

    wip: version and the video dont add up

--- src/components/CreateTweet.tsx
+++ src/components/CreateTweet.tsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
-import { appRouter } from "../server/api/root"
+import { appRouter } from "../server/api/root";
 
 export function CreateTweet() {
     const [text, setText] = useState("")
+    const [error, setError] = useState("")
 
-    const mutation = appRouter.tweet.create.useMutate
+    const { mutationAsync } = appRouter.tweet.create.useMutation()
 
     const handleSubmit = (e) => {
         e.preventDefault()
