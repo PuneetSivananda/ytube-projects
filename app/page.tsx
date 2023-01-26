6e81d6b6c11fa9ebb796b1066130eee31b0fcdf3
PuneetSivananda <puneet.sivananda@gmail.com>
2023-01-26 22:21:43 +0530

    feat: sidebar layout sample code

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,11 @@ import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <div className='flex w-full'>
-      <div className="w-1/5 h-16 bg-red-500">
+    <div className='flex min-h-screen'>
+      <div className="w-1/5 bg-red-500">
         SideBar
       </div>
-      <div className="w-4/5 h-16 bg-green-500">
+      <div className="w-4/5 bg-green-500">
         Main Content
       </div>
     </div>
