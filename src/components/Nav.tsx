0a2ecf9b7970d87fbe5a6c8f48def0950f6a77b2
PuneetSivananda <puneet.sivananda@gmail.com>
2023-01-31 14:06:32 +0530

    feat: nav bar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,10 +1,16 @@
+import { NavLink } from "@solidjs/router";
 import { Component } from "solid-js";
 
 const Nav: Component = () => {
     return (
-        <div>
-            <h1>Nav Component</h1>
-        </div>
+        <nav class="mt-5 mb-3">
+            <NavLink href="/" class="btn btn-blue mr-2" end activeClass="btn-success">
+                Home
+            </NavLink>
+            <NavLink href="/savedrepos" class="btn btn-blue mr-2" activeClass="btn-success">
+                Saved Repos ~ 5
+            </NavLink>
+        </nav >
     )
 }
 
