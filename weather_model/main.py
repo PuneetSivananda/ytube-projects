6d9242acce564fbdd18cc942c8694702207f9870
Puneet Sivananda <puneet.sivananda@tensoriot.com>
2023-02-01 12:39:54 +0530

    feat: updated the code for the plotting

--- weather_model/main.py
+++ weather_model/main.py
@@ -7,11 +7,24 @@ A = np.array([
     [0.25, 0, 0.25],
     [0.25, 0.5, 0.5]
 ])
-print(A)
+
 
 xtoday = np.array([[1], [0], [0]])
-print(xtoday)
+
+# @ column multiplication
 
 print(A @ xtoday)
+the_weather = np.zeros((3,50))
+the_weather[:,0] = xtoday[:,0]
+for k in range(50):
+    xtomorrow = A @ xtoday
+    the_weather[:,k] = xtomorrow[:,0]
+    print(k)
+    print(xtomorrow)
+    xtoday = xtomorrow
 
-for k in range()
+print(the_weather)
+
+plt.plot(the_weather.transpose(),)
+plt.grid(True)
+plt.show()
