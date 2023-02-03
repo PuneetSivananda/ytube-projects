1a5503f6e636a99d119445ffbf4237c1e57e1a29
Puneet Sivananda <puneet.sivananda@tensoriot.com>
2023-02-03 11:09:50 +0530

    feat: exponential compound interest

--- simple_differential_equation/simple_diffrential_eqn.py
+++ simple_differential_equation/simple_diffrential_eqn.py
@@ -2,4 +2,15 @@ import numpy as np
 
 r =  0.05
 
+# Simple compound
 print(1+r)
+
+# compound monthly
+compund_with_interest = (1+r/12)**12
+print(compund_with_interest)
+
+# compound yearly 
+compund_with_interest_yearly = (1+r/365)**365
+print(compund_with_interest_yearly)
+
+print(np.exp(r))
