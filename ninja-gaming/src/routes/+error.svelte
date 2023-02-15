0f68dd287366184162ac7bb9beed69faeef21b35
PuneetSivananda <puneet.sivananda@gmail.com>
2023-02-15 23:41:44 +0530

    feat: using error message

---
+++ ninja-gaming/src/routes/+error.svelte
@@ -0,0 +1,29 @@
+<script context="module">
+	// @ts-ignore
+	export function load({ error }) {
+		return {
+			props: { message: error.message }
+		};
+	}
+</script>
+
+<script>
+	/**
+	 * @type {any}
+	 */
+	export let message;
+	console.log(message);
+</script>
+
+<div class="error">
+	<h3>Oops</h3>
+	<p>{message}</p>
+	<a href="/guides">View all Guides</a>
+</div>
+
+<style>
+	.error {
+		text-align: center;
+		margin-top: 20%;
+	}
+</style>
