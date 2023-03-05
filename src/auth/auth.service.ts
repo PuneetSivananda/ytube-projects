b1f7019f67ee866c56a26151c9b5abdd2f8368d6
PuneetSivananda <puneet.sivananda@gmail.com>
2023-03-05 23:36:02 +0530

    feat: exporting modules

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { User, Bookmark } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable({})
 export class AuthService {
+  // Dependency Injection
+  constructor(private prisma: PrismaService) {}
   login() {}
   singup() {
     return { msg: 'I have signed up' };
