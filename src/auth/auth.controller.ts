5d1deedd5726c17dd3cc56d10e3e87b5b6f8c6df
PuneetSivananda <puneet.sivananda@gmail.com>
2023-03-05 23:54:33 +0530

    feat: implementation for req body integration

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Post, Req } from '@nestjs/common';
+import { Request } from 'express';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
@@ -6,7 +7,10 @@ export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signup() {
+  signup(@Req() req: Request) {
+    // Validation Process here
+    // Use Dtos
+    console.log(req.body);
     return this.authService.singup();
   }
 
