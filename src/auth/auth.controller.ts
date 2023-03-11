4e10d73d35c7b8e3c48b992ee6e1596e5104b8a0
PuneetSivananda <puneet.sivananda@gmail.com>
2023-03-11 21:56:39 +0530

    feat: introduce dtos

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,16 +1,16 @@
-import { Controller, Post, Req } from '@nestjs/common';
-import { Request } from 'express';
+import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signup(@Req() req: Request) {
+  signup(@Body() dto: AuthDto) {
     // Validation Process here
     // Use Dtos
-    console.log(req.body);
+    console.log({ dto });
     return this.authService.singup();
   }
 
