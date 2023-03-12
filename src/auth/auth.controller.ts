b8b9dd3a888ce41dab076558e8fa8e910e86970f
PuneetSivananda <puneet.sivananda@gmail.com>
2023-03-12 23:50:36 +0530

    feat: sending updated response headers

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,6 +3,8 @@ import {
   Post,
   Body,
   ParseIntPipe,
+  HttpCode,
+  HttpStatus,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
@@ -18,8 +20,9 @@ export class AuthController {
     return this.authService.signup(dto);
   }
 
+  @HttpCode(HttpStatus.OK)
   @Post('signin')
   signin(@Body() dto: AuthDto) {
-     return this.authService.signin(dto);
+    return this.authService.signin(dto);
   }
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -8,16 +8,11 @@ import { User } from '@prisma/client';
 import { GetUser } from 'src/auth/decorator';
 import { JWTGuard } from 'src/auth/guard';
 
+@UseGuards(JWTGuard) // Custom that extends the authguard provided by nestjs; can use this on the entire controller as well
 @Controller('users')
 export class UserController {
-  @UseGuards(JWTGuard) // Custom that extends the authguard provided by nestjs; can use this on the entire controller as well
   @Get('me')
-  getMe(
-    @GetUser() user: User,
-    @GetUser('id') id: number,
-    @GetUser('email') email: string,
-  ) {
-    console.log({ id, email });
+  getMe(@GetUser() user: User) {
     return user;
   }
 }
