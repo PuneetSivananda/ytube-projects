673df65cde6f801d496d53005a694bffc22b5aac
PuneetSivananda <puneet.sivananda@gmail.com>
2023-03-12 22:37:12 +0530

    feat: setup jwt strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
-import { PrismaModule } from 'src/prisma/prisma.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtStrategy } from './strategy';
 
 // Dependency Injection
 @Module({
-  imports: [JwtModule.register({ })],
+  imports: [JwtModule.register({})],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
 })
 class AuthModule {}
 
