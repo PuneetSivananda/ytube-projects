e14212bd0330ab9aa814e40229cd30e9d50778c2
Puneet Sivananda <puneet.sivananda@tensoriot.com>
2023-03-16 11:25:12 +0530

    feat: setting up the tests for test db

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,12 +1,29 @@
+import { INestApplication } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 import { AppModule } from '../src/app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 describe('App e2e', () => {
+  let app: INestApplication;
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
+    // create app
+    app = moduleRef
+      .createNestApplication()
+      .useGlobalPipes(
+        new ValidationPipe({ whitelist: true }),
+      );
+    // start the app
+    await app.init();
   });
+  // testing logic
   it.todo('should pass');
-  it.todo('should pass 2');
+  // connect to test database
+
+  // close the app
+  afterAll(() => {
+    app.close();
+  });
 });
