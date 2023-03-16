440d8ca572a1b85a4c0e7c1166198be1de92c6df
Puneet Sivananda <puneet.sivananda@tensoriot.com>
2023-03-16 14:59:39 +0530

    feat: running test cases

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,11 +1,17 @@
 import { INestApplication } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
-import { AppModule } from '../src/app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as pactum from 'pactum';
+
+import { AppModule } from '../src/app.module';
+import { PrismaService } from '../src/prisma/prisma.service';
+import { AuthDto } from '../src/auth/dto';
 
 describe('App e2e', () => {
   let app: INestApplication;
+  let prisma: PrismaService;
   beforeAll(async () => {
+    //reset the database
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
@@ -17,13 +23,78 @@ describe('App e2e', () => {
       );
     // start the app
     await app.init();
+    await app.listen(3333);
+    pactum.request.setBaseUrl('http://localhost:3333'); // providing the base url
+    prisma = app.get(PrismaService);
+    await prisma.cleandb();
   });
   // testing logic
-  it.todo('should pass');
-  // connect to test database
 
   // close the app
   afterAll(() => {
     app.close();
+  });
+
+  describe('Auth', () => {
+    const dto: AuthDto = {
+      email: 'user@gmail.com',
+      password: '123',
+    };
+    describe('SignUp', () => {
+      it('should throw if email is empty', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody({
+            password: dto.password,
+          })
+          .expectStatus(400);
+      });
+
+      it('should throw if password is empty', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody({
+            email: dto.email,
+          })
+          .expectStatus(400);
+      });
+
+      it('should should signup', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody(dto)
+          .expectStatus(201);
+      });
+    });
+    describe('SignIn', () => {
+      it('should signin', () => {
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .withBody(dto)
+          .expectStatus(200);
+      });
+    });
+  });
+
+  describe('User', () => {
+    describe('Get Me', () => {});
+
+    describe('Edit User', () => {});
+  });
+
+  describe('Bookmarks', () => {
+    describe('Create bookmark', () => {});
+
+    describe('Get Bookmark', () => {});
+
+    describe('Get Bookmark by id', () => {});
+
+    describe('Edit Bookmark', () => {});
+
+    describe('Delete bookmark', () => {});
   });
 });
