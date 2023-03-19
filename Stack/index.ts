1b901c5dadbae7cf50545f08be3787c239cd81f4
PuneetSivananda <puneet.sivananda@gmail.com>
2023-03-19 16:24:24 +0530

    check if palindrome

--- Stack/index.ts
+++ Stack/index.ts
@@ -1,7 +1,20 @@
 /**
  * Stacks
- * 
- * functions: push, pop, peek, length 
+ *
+ * functions: push, pop, peek, length
  */
 
-const letters = []
+const letters: string[] = [];
+
+var word = "racecar";
+var rword = "";
+
+for (var i = 0; i < word.length; i++) {
+  letters.push(word[i]);
+}
+
+for (var i = 0; i < word.length; i++) {
+  rword += letters.pop();
+}
+
+rword === word ? console.log("Palindrome") : console.log("Not a palindrome");
