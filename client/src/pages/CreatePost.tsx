b8352531df5dc7a7ed4d4eb997f60ffe5a723f5d
PuneetSivananda <puneet.sivananda@gmail.com>
2023-03-26 23:23:38 +0530

    feat: crate page layout

--- client/src/pages/CreatePost.tsx
+++ client/src/pages/CreatePost.tsx
@@ -1,4 +1,31 @@
-const CreatePost = () => {
-  return <></>;
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { preview } from '../assets';
+import { getRandomPrompt } from '../utils';
+import { FormField, Loader } from '../components';
+
+const CreatePost: React.FC = () => {
+  const navigate = useNavigate();
+  const [form, setForm] = useState({
+    name: '',
+    prompt: '',
+    photo: '',
+  });
+  const [generatingImg, setGeneratingImg] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  return (
+    <section className="max-w-7xl mx-auto">
+      <div>
+        <h1 className="font-extrabold text-[#222328] text-[32px]">Create</h1>
+        <p className="mt-2 text-[#666e75] text-[16px] max-w-[500px]">
+          Create imaginative and visulally stunning images through DALE-AI and
+          share them to the community
+        </p>
+      </div>
+    </section>
+  );
 };
+
 export default CreatePost;
