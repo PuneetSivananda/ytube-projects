072cac3777bf5fcbb2e854bed5d6aede3528852c
PuneetSivananda <puneet.sivananda@gmail.com>
2023-03-27 19:47:38 +0530

    feat: the skeleton from the api server

--- server/index.js
+++ server/index.js
@@ -2,20 +2,32 @@ import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
 
+import connectDB from "./mongodb/connect.js";
+import PostRoutes from "./routes/postRoutes.js";
+import DaleRoutes from "./routes/daleRoutes.js";
+
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 
+app.use("/api/v1/post", PostRoutes);
+app.use("/api/v1/dalle", DaleRoutes);
+
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
 const startServer = async () => {
-  app.listen(8080, () => {
-    console.log("Server has started on port: 8080");
-  });
+  try {
+    connectDB(process.env.MONGODB_URL);
+    app.listen(8080, () => {
+      console.log("Server has started on port: 8080");
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 startServer();
