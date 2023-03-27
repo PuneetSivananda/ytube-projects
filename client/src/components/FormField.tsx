540cc226f0e1e0122a5d278d8bb64010f802df27
PuneetSivananda <puneet.sivananda@gmail.com>
2023-03-27 18:30:56 +0530

    feat: frontend code for the app

--- client/src/components/FormField.tsx
+++ client/src/components/FormField.tsx
@@ -1,5 +1,54 @@
 import React from 'react';
 
-const FormField = () => <div></div>;
-
+const FormField: React.FC = ({
+  LabelName,
+  type,
+  name,
+  placeholder,
+  value,
+  handleChange,
+  isSurpriseMe,
+  handleSupriseMe,
+}: {
+  LabelName: any;
+  type: string;
+  name: string;
+  placeholder: string;
+  value: string;
+  handleChange: () => void;
+  isSurpriseMe: boolean;
+  handleSupriseMe: () => void;
+}) => {
+  return (
+    <div>
+      <div className="flex items-center gap-2 mb-2">
+        <label
+          htmlFor={name}
+          className="block text-sm font-medium text-gray-900"
+        >
+          {LabelName}
+        </label>
+        {isSurpriseMe && (
+          <button
+            type="button"
+            onClick={handleSupriseMe}
+            className="font-semibold text-xs bg-[#ECECF1] py-1 px-2 rounded-[5px] text-black"
+          >
+            Surprise Me
+          </button>
+        )}
+      </div>
+      <input
+        type={type}
+        name={name}
+        id={name}
+        placeholder={placeholder}
+        value={value}
+        onChange={handleChange}
+        required
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#4649ff] focus:border-[#4649ff] outline-none block w-full p-3"
+      />
+    </div>
+  );
+};
 export default FormField;
