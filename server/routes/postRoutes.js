47cffbf4117b880ac9b4e1a06157bb27af5361c3
PuneetSivananda <puneet.sivananda@gmail.com>
2023-03-27 23:20:38 +0530

    feat: backend is done

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -7,4 +7,48 @@ dotenv.config();
 
 const router = express.Router();
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+//Get all posts
+router.route("/").get(async (req, res) => {
+  try {
+    const posts = await Post.find({});
+    res.status(200).json({
+      success: true,
+      data: posts,
+    });
+  } catch (e) {
+    res.status(500).json({
+      success: false,
+      message: e,
+    });
+  }
+});
+
+//Create a post route
+router.route("/").post(async (req, res) => {
+  try {
+    const { name, prompt, photo } = req.body;
+    const photo_url = await cloudinary.uploader.upload(photo);
+    const newPost = await Post.create({
+      name,
+      prompt,
+      photo: photo_url.url,
+    });
+    res.status(201).json({
+      success: true,
+      data: newPost,
+    });
+  } catch (e) {
+    res.status(500).json({
+      success: false,
+      message: e,
+    });
+  }
+});
+
 export default router;
