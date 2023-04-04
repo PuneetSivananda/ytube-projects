e953d7d75c1e1a8e00ce2fddcb7a7524dbf4781d
PuneetSivananda <puneet.sivananda@gmail.com>
2023-04-04 22:48:16 +0530

    feat: refactor codebase

--- 0.SetupCode/main.cpp
+++ 0.SetupCode/main.cpp
@@ -7,5 +7,10 @@ int main(int argc, char **argv)
     std::cout<<std::endl;
     value+=5;
     std::cout<<"The Value after +5 is: "<<value<<std::endl;
+
+
+    std::cout << "Formatter Table: " << std::endl;
+
+    std::cout << std::setw(10) << "Last Name" << std::setw(10) << "First Name" << std::setw(10) << "Age" << std::endl;
     return 0;
 }
---
+++ 1.Formatting/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << std::endl;
+    std::cout << "Hello";
+    std::cout << std::endl;
+    std::cout << "World" << std::endl;
+    return 0;
+}
