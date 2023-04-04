7abf5fa9bff4ac8074e8985d13bab1d76ae91ebf
PuneetSivananda <puneet.sivananda@gmail.com>
2023-04-04 23:01:37 +0530

    feat: formatting text

--- 1.Formatting/main.cpp
+++ 1.Formatting/main.cpp
@@ -10,5 +10,12 @@ int main()
 
     std::cout << "Flushes the buffer when called" << std::endl
               << std::flush;
+
+    int col_width{10};
+
+    std::cout << std::right;
+    std::cout << std::setw(col_width) << "Lastname" << std::setw(col_width) << "Firstname" << std::setw(4) << std::endl;
+    std::cout << std::setw(col_width) << "Moore" << std::setw(col_width) << "Roger" << std::setw(4) << std::endl;
+    std::cout << std::setw(col_width) << "Smith" << std::setw(col_width) << "Dave" << std::setw(4) << std::endl;
     return 0;
 }
