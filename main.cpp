911a269bf18387e53165e573d69daf7327ce4da9
PuneetSivananda <puneet.sivananda@gmail.com>
2023-04-04 22:19:51 +0530

    feat: char pointers

--- main.cpp
+++ main.cpp
@@ -2,8 +2,9 @@
 
 int main(int argc, char **argv)
 {
+    char * p_message {"Hello From Memory"};
     std::cout << "Hello World" << std::endl;
-    int value = 7/0;
-    std::cout << value << std::endl;
+    std::cout << p_message << std::endl;
+    std::cout << * p_message << std::endl;
     return 0;
 }
