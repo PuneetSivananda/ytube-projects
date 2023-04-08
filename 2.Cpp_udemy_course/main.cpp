7b4c8812e83ffb385fa8c5195a1ada6b571ef5dd
PuneetSivananda <puneet.sivananda@gmail.com>
2023-04-08 22:43:47 +0530

    feat: updates

--- 2.Cpp_udemy_course/main.cpp
+++ 2.Cpp_udemy_course/main.cpp
@@ -1,9 +1,18 @@
-#include "stdio.h"
 #include <iostream>
+#include <string>
+
+using namespace std;
 
 int main()
 {
+    int myFirsint = 5;
+    float myFirstFloat = 3.4f;
+    bool myFirstBool = true;
+    char myFirstChar = 'a'; // single quote for chars
+    std::string myFirstStr = "hello";
+    string mySecondString = "My second string";
     std::cout << "Hello World!" << std::endl;
+    double myFirstDouble = 2.3;
     system("read -p 'Press Enter to continue...' var");
     return 0;
 }
