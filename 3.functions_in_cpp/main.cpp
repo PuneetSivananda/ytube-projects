b1ca133b1a43e00b58a9492aff5cbb4e6d4eb62f
PuneetSivananda <puneet.sivananda@gmail.com>
2023-05-13 11:13:04 +0530

    feat: cpp functions

--- 3.functions_in_cpp/main.cpp
+++ 3.functions_in_cpp/main.cpp
@@ -15,12 +15,35 @@ bool IsNumberSix(int number)
     }
 }
 
+void GreetPerson(string name)
+{
+    cout << "Hi " << name << "!" << endl;
+    printf("print a number: %i", 4);
+}
+
+bool WantToGreet(string name)
+{
+    if (name == "Link")
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
 int main()
 {
     string result;
     result = (IsNumberSix(6) == 1) ? "Yes" : "No";
     cout << result << endl;
     result = (IsNumberSix(4) == 1) ? "Yes" : "No";
+    cout << result << endl;
+
+    GreetPerson("Bob");
+    cout << endl;
+    result = (WantToGreet("Bob") == 1) ? "Yes" : "No";
     cout << result << endl;
     return 0;
 }
