9d7994c6e3a3049f5b50f3bf826c3a50bab4d56c
PuneetSivananda <puneet.sivananda@gmail.com>
2023-05-27 16:22:18 +0530

    feat: setup sample code

---
+++ golang/2.valid_anagram/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func isAnagram(s string, t string) bool {
+	return true
+}
+
+func main() {
+	var stringA string
+	var stringB string
+	fmt.Println("Enter First String:")
+	fmt.Scanln(&stringA)
+	fmt.Println("Enter Second String:")
+	fmt.Scanln(&stringB)
+
+	fmt.Println(stringA, stringB)
+}
