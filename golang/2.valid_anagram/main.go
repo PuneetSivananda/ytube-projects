9b887d8ffa03fa3ec8499b71277b167901ee4086
PuneetSivananda <puneet.sivananda@gmail.com>
2023-05-27 16:58:30 +0530

    feat: anagram implementation

--- golang/2.valid_anagram/main.go
+++ golang/2.valid_anagram/main.go
@@ -3,6 +3,28 @@ package main
 import "fmt"
 
 func isAnagram(s string, t string) bool {
+	if len(s) != len(t) {
+		return false
+	}
+
+	string_count := make(map[rune]int)
+	for _, ch := range s {
+		string_count[ch]++
+	}
+
+	for _, ch := range t {
+		string_count[ch]--
+		if string_count[ch] < 0 {
+			return false
+		}
+	}
+
+	for _, count := range string_count {
+		if count != 0 {
+			return false
+		}
+	}
+
 	return true
 }
 
@@ -14,5 +36,5 @@ func main() {
 	fmt.Println("Enter Second String:")
 	fmt.Scanln(&stringB)
 
-	fmt.Println(stringA, stringB)
+	fmt.Println(isAnagram(stringA, stringB))
 }
