0aeb33bddd46f83d0ca7c8e06fd154d70cfb3351
PuneetSivananda <puneet.sivananda@gmail.com>
2023-06-08 21:04:46 +0530

    feat: go channels runthough

--- golang/5.go_channels/main.go
+++ golang/5.go_channels/main.go
@@ -1,19 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-func SendValue(c chan int) {
-	c <- 9
+func SendValue(c chan string) {
+	fmt.Println("Executin Goroutine")
+	time.Sleep(1 * time.Second)
+	c <- "Hello World"
+	fmt.Println("Finished Executin Goroutine")
 }
 
 func main() {
 	fmt.Println("Channels Tutorial")
-	values := make(chan int)
+	values := make(chan string, 2) //buffered channels
 	defer close(values)
 
+	go SendValue(values)
 	go SendValue(values)
 	value := <-values
 
 	fmt.Println(value)
+
+	time.Sleep(1 * time.Second)
 
 }
