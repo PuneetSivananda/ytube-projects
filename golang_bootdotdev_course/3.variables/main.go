9d2e17b4b981f00d49812d57a601d4f37f40eb98
Puneet Sivananda <puneet.sivananda@tensoriot.com>
2023-06-12 11:53:59 +0530

    feat: typecasting integers in go

---
+++ golang_bootdotdev_course/3.variables/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	accountAge := 2.6
+
+	//create accountAgeInt here
+	accountAgeInt := int(accountAge)
+
+	fmt.Println("Your account has existed for", accountAgeInt, "years")
+}
