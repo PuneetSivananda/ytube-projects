b752599c76880b76c9f7308f1136955b86690f1a
Puneet Sivananda <puneet.sivananda@tensoriot.com>
2023-06-19 15:08:40 +0530

    feat: using waitgroups in go

--- golang/6.go_waitgroups/main.go
+++ golang/6.go_waitgroups/main.go
@@ -2,21 +2,35 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 	"sync"
-	"time"
 )
 
-func myFunc(wg *sync.WaitGroup) {
-	time.Sleep(1 * time.Second)
-	fmt.Println("Finished executing goroutine")
-	wg.Done()
+var urls = []string{
+	"https://google.com",
+	"https://twitter.com",
+	"https://tutorialsedge.net",
+}
+
+func fetchStatus(w http.ResponseWriter, r *http.Request) {
+	var wg sync.WaitGroup
+	for _, url := range urls {
+		wg.Add(1)
+		go func(url string) {
+			resp, err := http.Get(url)
+			if err != nil {
+				fmt.Fprintf(w, "%+v\n", err)
+			}
+			fmt.Fprintf(w, "%+v\n", resp.Status)
+			wg.Done()
+		}(url)
+	}
+	wg.Wait()
 }
 
 func main() {
 	fmt.Println("Go waitgroup tutorial")
-	var wg sync.WaitGroup
-	wg.Add(1)
-	go myFunc(&wg)
-	wg.Wait()
-	fmt.Println("Finished executing my go program")
+	http.HandleFunc("/", fetchStatus)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
