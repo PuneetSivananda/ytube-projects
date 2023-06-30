ba905f1ed6da8fe0833060f5bbc144b636cf3bfa
PuneetSivananda <puneet.sivananda@gmail.com>
2023-06-30 19:40:31 +0530

    feat: setting up theme provider

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,18 @@
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { themeSettings } from "@/theme";
+
 function App() {
+  const theme = useMemo(() => createTheme(themeSettings), []);
   return (
     <>
-      <div className="app"></div>
+      <div className="app">
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          Hello
+        </ThemeProvider>
+      </div>
     </>
   );
 }
---
+++ client/src/expanded-theme.ts
@@ -0,0 +1,11 @@
+// eslint-disable-next-line
+import { Pallete, PalleteColor } from "@mui/material/styles/createPalette";
+
+declare module "@mui/material/styles/createPalette" {
+  interface PalleteColor {
+    [key: number]: string;
+  }
+  interface Pallete {
+    tertiary: PalleteColor;
+  }
+}
