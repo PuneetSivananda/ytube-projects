9412cbe08389ad5665c20cbf3d6f7753b48798a4
PuneetSivananda <puneet.sivananda@gmail.com>
2023-06-30 19:56:34 +0530

    feat: create initial scnes, components folder structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,30 @@
-import { ThemeProvider, CssBaseline } from "@mui/material";
+import { ThemeProvider, CssBaseline, Box } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
 import { useMemo } from "react";
 import { themeSettings } from "@/theme";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "@/scenes/navbar";
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), []);
   return (
     <>
       <div className="app">
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          Hello
-        </ThemeProvider>
+        <BrowserRouter>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+              <Navbar />
+              <Routes>
+                <Route path="/" element={<div> Dashboard Page. </div>} />
+                <Route
+                  path="/predictions"
+                  element={<div> Predictions Page. </div>}
+                />
+              </Routes>
+            </Box>
+          </ThemeProvider>
+        </BrowserRouter>
       </div>
     </>
   );
