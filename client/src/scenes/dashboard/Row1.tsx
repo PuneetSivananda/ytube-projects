4e40add390c0856bf6628c5ce69c2d2508844e2e
PuneetSivananda <puneet.sivananda@gmail.com>
2023-07-03 23:50:53 +0530

    feat: updated the recharts to use gradient

--- client/src/scenes/dashboard/Row1.tsx
+++ client/src/scenes/dashboard/Row1.tsx
@@ -41,15 +41,57 @@ const Row1 = (props: Props) => {
             height={400}
             data={revenueExpenses}
             margin={{
-              top: 10,
-              right: 30,
-              left: 0,
-              bottom: 0,
+              top: 15,
+              right: 25,
+              left: -10,
+              bottom: 60,
             }}
           >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <YAxis />
+            {/* <CartesianGrid strokeDasharray="3 3" /> */}
+            <defs>
+              <linearGradient id="colorRevenue" x1="0" y1="0" x2="0" y2="1">
+                <stop
+                  offset="5%"
+                  stopColor={palette.primary[300]}
+                  stopOpacity={0.5}
+                />
+                <stop
+                  offset="95%"
+                  stopColor={palette.primary[300]}
+                  stopOpacity={0}
+                />
+              </linearGradient>
+
+              <linearGradient id="colorExpenses" x1="0" y1="0" x2="0" y2="1">
+                <stop
+                  offset="5%"
+                  stopColor={palette.primary[300]}
+                  stopOpacity={0.5}
+                />
+                <stop
+                  offset="95%"
+                  stopColor={palette.primary[300]}
+                  stopOpacity={0}
+                />
+              </linearGradient>
+            </defs>
+            <XAxis
+              dataKey="name"
+              tickLine={false}
+              style={{
+                fontSize: "10px",
+              }}
+            />
+            <YAxis
+              tickLine={false}
+              axisLine={{
+                strokeWidth: 0,
+              }}
+              style={{
+                fontSize: "10px",
+              }}
+              domain={[8000, 23000]}
+            />
             <Tooltip />
             <Area
               type="monotone"
