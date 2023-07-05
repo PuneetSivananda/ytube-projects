553b8d751817d9ffb420b46b90c8bdb52e82079e
PuneetSivananda <puneet.sivananda@gmail.com>
2023-07-05 11:17:00 +0530

    feat: completed the entire card for piechart

--- client/src/scenes/dashboard/Row2.tsx
+++ client/src/scenes/dashboard/Row2.tsx
@@ -15,7 +15,8 @@ import {
   XAxis,
   YAxis,
 } from "recharts";
-import { useTheme } from "@mui/material";
+import { Box, Typography, useTheme } from "@mui/material";
+import FlexBetween from "@/components/FlexBetween";
 
 const pieData = [
   { name: "Group A", value: 600 },
@@ -103,29 +104,51 @@ const Row2 = () => {
         </ResponsiveContainer>
       </DashboardBox>
       <DashboardBox gridArea="e">
-        <PieChart
-          width={110}
-          height={100}
-          margin={{
-            top: 0,
-            right: -10,
-            left: 10,
-            bottom: 0,
-          }}
-        >
-          <Pie
-            stroke="none"
-            data={pieData}
-            innerRadius={18}
-            outerRadius={38}
-            paddingAngle={2}
-            dataKey="value"
+        <BoxHeader title="Campaingns and Targets" sideText="+4%" />
+        <FlexBetween mt="0.25rem" gap="1.5rem" paddingRight="1rem">
+          <PieChart
+            width={110}
+            height={100}
+            margin={{
+              top: 0,
+              right: -10,
+              left: 10,
+              bottom: 0,
+            }}
           >
-            {pieData.map((entry, index) => (
-              <Cell key={`cell-${index}`} fill={pieColors[index]} />
-            ))}
-          </Pie>
-        </PieChart>
+            <Pie
+              stroke="none"
+              data={pieData}
+              innerRadius={18}
+              outerRadius={38}
+              paddingAngle={2}
+              dataKey="value"
+            >
+              {pieData.map((entry, index) => (
+                <Cell key={`cell-${index}`} fill={pieColors[index]} />
+              ))}
+            </Pie>
+          </PieChart>
+          <Box ml="-0.7rem" flexBasis="40%" textAlign="center">
+            <Typography variant="h5">Target Sales</Typography>
+            <Typography m="0.3rem 0" variant="h3" color={palette.primary[300]}>
+              83
+            </Typography>
+            <Typography variant="h6">
+              Finance Goals of the Campaign that is desired
+            </Typography>
+          </Box>
+          <Box flexBasis="40%">
+            <Typography variant="h5">Losses in Revenue</Typography>
+            <Typography variant="h6">Losses are down 25%</Typography>
+            <Typography mt="0.4rem" variant="h5">
+              Profit Margins
+            </Typography>
+            <Typography variant="h6">
+              Margins are up by 30% from last month.
+            </Typography>
+          </Box>
+        </FlexBetween>
       </DashboardBox>
       <DashboardBox gridArea="f"></DashboardBox>
     </>
