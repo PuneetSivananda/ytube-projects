76dc24ce9114380cb1b60bd12f3fa9cc8c66d9a3
PuneetSivananda <puneet.sivananda@gmail.com>
2023-07-06 23:22:46 +0530

    feat: documenting the code method

--- neet-code/1.contains-duplicate/golang/main.go
+++ neet-code/1.contains-duplicate/golang/main.go
@@ -2,6 +2,25 @@ package main
 
 import "fmt"
 
+/**
+ Create a Set structure in Golang
+ Add ability to Add, Remove and Contains functionality to the Set structure
+
+	implement the logic using:
+		for every number in nums
+		if num is present in the hashmap return True
+			stop
+		else
+			add the num to the hashmap
+
+		end of for loop return False
+*/
+
 func main() {
-	fmt.Println("Hello World")
+	s := make(map[string]interface{})
+	var exists = struct{}{}
+	s["James"] = exists
+	for v := range s {
+		fmt.Println(v)
+	}
 }
