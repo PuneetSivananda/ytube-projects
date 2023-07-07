ab5d4ffbc13ca3e32b547345f10955c53ce833f0
PuneetSivananda <puneet.sivananda@gmail.com>
2023-07-07 22:38:06 +0530

    chore: updates

--- neet-code/3.two-sum/python/main.py
+++ neet-code/3.two-sum/python/main.py
@@ -1,13 +1,13 @@
 class Solution:
     def check_combination_brute_force(self, input_array, target):
         for value in range(len(input_array)):
-            for val in range(len(input_array)-1): 
+            for val in range(len(input_array)):
                 if(input_array[val]==input_array[value]): 
-                    break
+                    break  
                 if(input_array[val]+input_array[value] == target):
                     print(input_array[value], input_array[val])
                     return True
         return False
     
 soln = Solution()
-print(soln.check_combination_brute_force([2, 7, 11, 15], 9))
+print(soln.check_combination_brute_force([2, 7, 11, 15], 18))
