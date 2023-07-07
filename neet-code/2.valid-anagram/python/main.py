890833eaeb2522af88dbc21f5837ba83bbb7eb49
PuneetSivananda <puneet.sivananda@gmail.com>
2023-07-07 13:34:05 +0530

    feat: check if anagram implementation

--- neet-code/2.valid-anagram/python/main.py
+++ neet-code/2.valid-anagram/python/main.py
@@ -11,3 +11,20 @@
     # convert string1 and string2 to hashmaps
     # compare the same keys are present in both the hashmaps
     # for each k,v in string1 hashmap compare the values of 2nd hashmap
+
+class Solution:
+    def isAnagram(self, s, t):
+        if(len(s)!=len(t)):
+            return False
+        countS, countT = {},{}
+        for i in range(len(s)):
+            countS[s[i]] = 1+countS.get(s[i], 0)# counting the occurences of each char
+            countT[t[i]] = 1+countT.get(t[i], 0)
+        for c in countS:
+            if countS[c] != countT.get(c,0):
+                return False
+            
+        return True
+    
+soln = Solution()
+print(soln.isAnagram("nagaram", "anagram"))
