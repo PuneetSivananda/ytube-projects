cf5f09a2ba81fbbdab288d2afa6bac4217c6a89f
PuneetSivananda <puneet.sivananda@gmail.com>
2023-07-07 22:33:36 +0530

    feat: brute force approach

--- neet-code/3.two-sum/python/main.py
+++ neet-code/3.two-sum/python/main.py
@@ -1,6 +1,13 @@
 class Solution:
-    def check_combination(self):
+    def check_combination_brute_force(self, input_array, target):
+        for value in range(len(input_array)):
+            for val in range(len(input_array)-1): 
+                if(input_array[val]==input_array[value]): 
+                    break
+                if(input_array[val]+input_array[value] == target):
+                    print(input_array[value], input_array[val])
+                    return True
         return False
     
 soln = Solution()
-print(soln.check_combination())
+print(soln.check_combination_brute_force([2, 7, 11, 15], 9))
