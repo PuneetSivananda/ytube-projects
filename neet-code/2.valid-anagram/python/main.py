9ab93ed062180647f7849077bf8faa19221983e2
PuneetSivananda <puneet.sivananda@gmail.com>
2023-07-07 13:25:20 +0530

    feat: logic

--- neet-code/2.valid-anagram/python/main.py
+++ neet-code/2.valid-anagram/python/main.py
@@ -1,2 +1,13 @@
 # check if 2 strings are anagrams of each other.
 # TODO: Group Anagrams 
+
+# Craete a Hashmap that stores each string, key: char, value: count
+# Now compare the 2 Hashmaps of the 2 Strings
+
+# method:
+    # check the length of 2 string if same process
+    # else
+    #  return strings do not match
+    # convert string1 and string2 to hashmaps
+    # compare the same keys are present in both the hashmaps
+    # for each k,v in string1 hashmap compare the values of 2nd hashmap
