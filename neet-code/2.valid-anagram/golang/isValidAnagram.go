2507f7869b76b819e02f5e4f3be35d7a1a023f71
PuneetSivananda <puneet.sivananda@gmail.com>
2023-07-08 22:41:57 +0530

    wip: updates to the code

--- neet-code/2.valid-anagram/golang/isValidAnagram.go
+++ neet-code/2.valid-anagram/golang/isValidAnagram.go
@@ -1,26 +1,36 @@
 package main
 
+import "fmt"
+
 type Map struct {
 	m map[rune]interface{}
 }
 
-var exists = struct{}{}
-
 func NewMap() *Map {
-	s := &Map{}
-	s.m = make(map[rune]interface{})
-	return s
+	m := &Map{}
+	m.m = make(map[rune]interface{})
+	return m
 }
 
-func (s *Map) Add(value rune) {
-	s.m[value] = exists
+func (m *Map) Add(key rune, value int) {
+	m.m[key] = value
 }
 
-func (s *Map) Remove(value rune) {
-	delete(s.m, value)
+func (m *Map) Remove(value rune) {
+	delete(m.m, value)
 }
 
-func (s *Map) Contains(value rune) bool {
-	_, c := s.m[value]
+func (m *Map) Contains(value rune) bool {
+	_, c := m.m[value]
 	return c
+}
+
+func isAnagram(str1, str2 string) bool {
+	m := NewMap()
+	fmt.Println(m)
+	return false
+}
+func main() {
+	result := isAnagram("nagaram", "anagram")
+	fmt.Println(result)
 }
