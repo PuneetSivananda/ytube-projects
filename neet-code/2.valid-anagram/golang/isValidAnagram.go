3c8c8aa21ab202a3e493769209e79475695bce0f
PuneetSivananda <puneet.sivananda@gmail.com>
2023-07-10 16:23:54 +0530

    wip: python code implementation

--- neet-code/2.valid-anagram/golang/isValidAnagram.go
+++ neet-code/2.valid-anagram/golang/isValidAnagram.go
@@ -26,8 +26,17 @@ func (m *Map) Contains(value rune) bool {
 }
 
 func isAnagram(str1, str2 string) bool {
-	m := NewMap()
-	fmt.Println(m)
+	if len(str1) != len(str2) {
+		return false
+	}
+	countS1 := NewMap()
+	countS2 := NewMap()
+	fmt.Println(countS1)
+	fmt.Println(countS2)
+	for _, s := range str1 {
+		fmt.Printf("%c %d\n", s, rune(s))
+		countS1.Add(s, 1+countS1.Contains(s))
+	}
 	return false
 }
 
