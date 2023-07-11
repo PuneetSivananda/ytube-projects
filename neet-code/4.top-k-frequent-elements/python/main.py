a77740b02e5c5e4fb5304775911bd25113191405
PuneetSivananda <puneet.sivananda@gmail.com>
2023-07-11 23:12:39 +0530

    feat: get the top k highest nums in a list

---
+++ neet-code/4.top-k-frequent-elements/python/main.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class Solution:
+    def top_k_frequent_elements(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+        freq = [[] for i in range(len(nums)+1)]
+        for n in nums:
+            count[n] = 1 + count.get(n, 0)
+        for n, c in count.items():
+            freq[c].append(n)
+        res = []
+        for i in range(len(freq) - 1, 0, -1):
+            for n in freq[i]:
+                res.append(n)
+                if (len(res) == k):
+                    return res
+
+
+soln = Solution()
+print(soln.top_k_frequent_elements([1, 1, 1, 2, 2, 4, 4, 4, 3], 2))
