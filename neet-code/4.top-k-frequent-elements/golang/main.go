5cb34928c34af2c7090c69a6f30f54fd44ff5add
PuneetSivananda <puneet.sivananda@gmail.com>
2023-07-12 11:07:07 +0530

    feat: implement golang version

--- neet-code/4.top-k-frequent-elements/golang/main.go
+++ neet-code/4.top-k-frequent-elements/golang/main.go
@@ -2,10 +2,19 @@ package main
 
 import "fmt"
 
-func printTopK() int {
+func printTopK(nums []int, k int) int {
+	m := make(map[int]int)
+	m[1] = 2
+	for k, v := range m {
+		fmt.Println(k, v)
+	}
+	for _, value := range nums {
+		fmt.Println(value)
+	}
 	return 0
 }
+
 func main() {
-	res := printTopK()
+	res := printTopK([]int{1, 1, 1, 2, 2, 4, 4, 4, 3}, 2)
 	fmt.Println(res)
 }
