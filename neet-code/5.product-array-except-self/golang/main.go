d0883d0c30a3e2e1cb3adb6ce5263e37f29bced8
PuneetSivananda <puneet.sivananda@gmail.com>
2023-07-13 20:58:48 +0530

    feat: compute prefix array

--- neet-code/5.product-array-except-self/golang/main.go
+++ neet-code/5.product-array-except-self/golang/main.go
@@ -4,6 +4,18 @@ import "fmt"
 
 func product_array_except_self(nums []int) {
 	fmt.Println(nums)
+	var res []int
+	for i := 0; i < len(nums); i++ {
+		res = append(res, 1)
+	}
+	prefix := 1
+	fmt.Println(prefix)
+	for i := 0; i < len(nums); i++ {
+		res[i] = prefix
+		prefix = prefix * nums[i]
+	}
+	fmt.Println(prefix)
+	fmt.Println(res)
 }
 
 func main() {
