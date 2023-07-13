9dfcb9ad9ed93c0d2859410c9566464fe198440f
PuneetSivananda <puneet.sivananda@gmail.com>
2023-07-13 20:54:19 +0530

    wip: setup golang code

--- neet-code/5.product-array-except-self/golang/main.go
+++ neet-code/5.product-array-except-self/golang/main.go
@@ -1,5 +1,12 @@
 package main
 
-func main(){
-	
+import "fmt"
+
+func product_array_except_self(nums []int) {
+	fmt.Println(nums)
+}
+
+func main() {
+	fmt.Println("Product array except self")
+	product_array_except_self([]int{1, 2, 3, 4})
 }
