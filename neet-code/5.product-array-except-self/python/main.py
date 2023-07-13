f34de0162169308408261bb1713c159c2fe274c7
PuneetSivananda <puneet.sivananda@gmail.com>
2023-07-13 20:39:30 +0530

    feat: initial code

---
+++ neet-code/5.product-array-except-self/python/main.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def product_array_except_self(self, nums: List[int], k: int) -> List[int]:
+        return ""
+
+
+soln = Solution()
+print(soln.product_array_except_self([1, 2, 2, 3, 4]))
