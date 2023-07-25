62c77869f52f24210d8e81deac9e7a3fd9a15f60
PuneetSivananda <puneet.sivananda@gmail.com>
2023-07-26 00:23:27 +0530

    wip: count sort algorithm taken reference

--- javascript-animations/count-sort/src/script.ts
+++ javascript-animations/count-sort/src/script.ts
@@ -31,6 +31,41 @@ function drawCountSortBars() {
   }
 }
 
+const countingSort = (arr) => {
+  const min = 0
+  const max = 0
+  const count = {};
+  
+  for (let i = min; i <= max; i++) {
+      count[i] = 0;
+  }
+  for (let i = 0; i < arr.length; i++) {
+      count[arr[i]] += 1;
+  }
+  /* Now, count is indexed by numbers, with values corresponding to occurrences, eg:
+   * {
+   *   3: 1,
+   *   4: 0,
+   *   5: 2,
+   *   6: 1,
+   *   7: 0,
+   *   8: 0,
+   *   9: 1
+   * }
+   */
+  
+  // Then, iterate over count's properties from min to max:
+  const sortedArr = [];
+  for (let i = min; i <= max; i++) {
+      while (count[i] > 0) {
+          sortedArr.push(i);
+          // redraw the array here
+          count[i]--;
+      }
+  }
+  return sortedArr;
+};
+
 // Count sort algorithm
 async function countSort() {
   for (let i = 0; i < bubbleSortArray.length - 1; i++) {
