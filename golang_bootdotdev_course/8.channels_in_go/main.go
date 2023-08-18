3c30ef1075978d866551755dbcd3fef4cc299ded
PuneetSivananda <puneet.sivananda@gmail.com>
2023-08-18 11:50:48 +0530

    chore: updates

--- golang_bootdotdev_course/8.channels_in_go/main.go
+++ golang_bootdotdev_course/8.channels_in_go/main.go
@@ -8,11 +8,9 @@ func foo(c chan int, someValue int) {
 
 func main() {
 	fooVal := make(chan int)
-	go foo(fooVal, 5)
-	go foo(fooVal, 3)
+	for i := 0; i < 10; i++ {
+		foo(fooVal, i)
+	}
 
-	// v1 := <-fooVal
-	// v2 := <-fooVal
-	v1, v2 := <-fooVal, <-fooVal
-	fmt.Println(v1, v2)
+	
 }
