50b10429ddcaab7a620f429c755537a2bed09ac3
PuneetSivananda <puneet.sivananda@gmail.com>
2023-08-23 19:37:01 +0530

    feat: implementation for the code

--- daily-coding-challenge/560.py
+++ daily-coding-challenge/560.py
@@ -3,3 +3,15 @@
 # For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 
 # Bonus: Can you do this in one pass?
+
+def findSum(arr, k):
+    for _, i in enumerate(arr):
+        for _, j in enumerate(arr):
+            if (i == j):
+                continue
+            if (i+j == k):
+                return True
+    return False
+
+
+print(findSum([10, 15, 3, 7], 18))
