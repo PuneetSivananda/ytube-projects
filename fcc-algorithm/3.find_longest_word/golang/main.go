229659844c1adc6a44c6a5921126351569f17919
PuneetSivananda <puneet.sivananda@gmail.com>
2023-08-24 14:53:15 +0530

    feat: completed the folang code

--- fcc-algorithm/3.find_longest_word/golang/main.go
+++ fcc-algorithm/3.find_longest_word/golang/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+func findLongestWord(sentence string) int {
+	MAX := 0
+	words := strings.Split(sentence, " ")
+	for word := 0; word < len(words); word++ {
+		if len(words[word]) > MAX {
+			MAX = len(words[word])
+		}
+	}
+	return MAX
+}
 
 func main() {
-	fmt.Println("Hello world")
+	fmt.Println(findLongestWord("The quick brown fox jumped over the lazy dog"))
 }
