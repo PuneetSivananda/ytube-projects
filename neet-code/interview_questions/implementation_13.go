fe4a0ade0d092f1396d958c9fdac7e8cd28384a3
PuneetSivananda <puneet.sivananda@gmail.com>
2023-08-28 17:48:25 +0530

    feat: make the loop from half of the items

--- neet-code/interview_questions/implementation_13.go
+++ neet-code/interview_questions/implementation_13.go
@@ -5,28 +5,27 @@ package main
 import "fmt"
 
 func isValid(s string) bool {
-	world := map[string]string{
-		"{": "}",
-		"(": ")",
-		"[": "]",
-	}
+	// world := map[string]string{
+	// 	"{": "}",
+	// 	"(": ")",
+	// 	"[": "]",
+	// }
 
 	top := len(s) - 1
-	for i := 0; i < len(s); i++ {
+	for i := 0; i < len(s)/2; i++ {
 		//check the val and map result
-		for j := top; j >= 0; j-- {
+		for j := top; j >= len(s)/2; j-- {
+			fmt.Println(i, j)
 			//fmt.Println(string(s[i]), string(s[j]))
 			//fmt.Println(world[string(s[i]))
-			fmt.Println(string(s[i]), string(s[j]), world[string(s[i])])
-			if world[string(s[i])] == string(s[j]) {
-				i = i + 1
-				j = j - 1
-				//	fmt.Println("match")
-				//	fmt.Println(i, j)
-			}
+			// fmt.Println(string(s[i]), string(s[j]), world[string(s[i])])
+			// if world[string(s[i])] == string(s[j]) {
+			// i = i + 1
+			// j = j - 1
+			//	fmt.Println("match")
+			//	fmt.Println(i, j)
 		}
 	}
-
 	return false
 }
 
