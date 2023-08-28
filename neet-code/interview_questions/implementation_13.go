ad6dc7d77db0c900a1eb737b308ee1f5e673b753
PuneetSivananda <puneet.sivananda@gmail.com>
2023-08-28 17:53:07 +0530

    chore: hmm

--- neet-code/interview_questions/implementation_13.go
+++ neet-code/interview_questions/implementation_13.go
@@ -5,17 +5,27 @@ package main
 import "fmt"
 
 func isValid(s string) bool {
-	// world := map[string]string{
-	// 	"{": "}",
-	// 	"(": ")",
-	// 	"[": "]",
-	// }
+	world := map[string]string{
+		"{": "}",
+		"(": ")",
+		"[": "]",
+	}
 
 	top := len(s) - 1
+	fmt.Println(world)
+
+	// these do the looping
+	// how about we do them manually and check for the values
+
 	for i := 0; i < len(s)/2; i++ {
 		//check the val and map result
 		for j := top; j >= len(s)/2; j-- {
 			fmt.Println(i, j)
+			fmt.Println(string(s[i]), string(s[j]))
+			if true {
+				i = i + 1
+				j = j - 1
+			}
 			//fmt.Println(string(s[i]), string(s[j]))
 			//fmt.Println(world[string(s[i]))
 			// fmt.Println(string(s[i]), string(s[j]), world[string(s[i])])
