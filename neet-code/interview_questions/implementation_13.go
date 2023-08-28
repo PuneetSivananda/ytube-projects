f34c6c7f33cda2173bf189806552afbd0ecf3f2c
PuneetSivananda <puneet.sivananda@gmail.com>
2023-08-28 16:56:27 +0530

    fix: the implementation code

---
+++ neet-code/interview_questions/implementation_13.go
@@ -0,0 +1,39 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import "fmt"
+
+func isValid(s string) bool {
+	world := map[string]string{
+		"{": "}",
+		"(": ")",
+		"[": "]",
+	}
+
+	top := len(s) - 1
+	for i := 0; i < len(s); i++ {
+		//check the val and map result
+		for j := top; j >= 0; j-- {
+			//fmt.Println(string(s[i]), string(s[j]))
+			//fmt.Println(world[string(s[i]))
+			fmt.Println(string(s[i]), string(s[j]), world[string(s[i])])
+			if world[string(s[i])] == string(s[j]) {
+				i = i + 1
+				j = j - 1
+				//	fmt.Println("match")
+				//	fmt.Println(i, j)
+			}
+		}
+	}
+
+	return false
+}
+
+func main() {
+	s1 := "[({})]"
+	//s2 := "([)]"
+	fmt.Println(isValid(s1))
+	//	fmt.Println(isValid(s2))
+
+}
